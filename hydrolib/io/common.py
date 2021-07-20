eae0d98d95cbf98e5909c5375b2fd88a75dd1185
tegelaer <maarten.tegelaers@deltares.nl>
2021-07-20 13:13:29 +0200

    Add LineReader Protocol as abstraction over Files

--- hydrolib/io/common.py
+++ hydrolib/io/common.py
@@ -1,9 +1,9 @@
 """common.py defines common constructs of the io module.
 """
 
-from enum import Enum
+from pathlib import Path
 from pydantic import BaseModel as PydanticBaseModel
-from typing import Optional, Tuple
+from typing import Optional, Protocol, Tuple, Union
 
 
 # TODO: Replace this BaseModel with the basemodel defined in basemodel.py once in main
@@ -21,3 +21,24 @@ class ParseMsg(BaseModel):
     line: Tuple[int, int]
     column: Optional[Tuple[int, int]]
     reason: str
+
+
+class LineReader(Protocol):
+    """FileReader describes the protocol to read lines from a file.
+
+    The File object matches this protocol, and thus can be used as
+    input for any function utilising the TextReader
+    """
+
+    def readline(self, size: int = -1) -> str:
+        """Read until newline or EOF and return a single str.
+
+        If the stream is already at EOF, an empty string is returned.
+
+        Args:
+            size (int, optional): Number of characters to read.
+                                            Defaults to -1.
+
+        Returns:
+            str: A string consisting of the specified number of characters.
+        """
