f78bd31a9b7881f861b42cc905b23a15a9b503ae
tegelaer <maarten.tegelaers@deltares.nl>
2021-07-20 11:53:38 +0200

    Add initial Parser logic

--- hydrolib/io/polyfile.py
+++ hydrolib/io/polyfile.py
@@ -1,8 +1,9 @@
-"""polyfil.py defines all classes and functions related to handling pol/pli(z) files.
+"""polyfily.py defines all classes and functions related to handling pol/pli(z) files.
 """
 
 from enum import Enum
 from hydrolib.io.common import ParseMsg, BaseModel
+from functools import reduce
 from typing import Callable, Dict, List, Optional, Sequence, Tuple
 
 
@@ -41,3 +42,240 @@ class PolyObject(BaseModel):
     description: Optional[Description]
     metadata: Metadata
     points: List[Point]
+
+
+class Block(BaseModel):
+    """Block is a temporary object which will be converted into a PolyObject."""
+    start_line: int
+
+    description: Optional[List[str]] = None
+    name: Optional[str] = None
+    dimensions: Optional[Tuple[int, int]] = None
+    points: Optional[List[Point]] = None
+
+    def get_description(self) -> Optional[Description]:
+        if self.description is not None:
+            return Description(content="\n".join(self.description))
+        else:
+            return None
+
+    def get_metadata(self) -> Optional[Metadata]:
+        if self.name is None or self.dimensions is None:
+            return None
+
+        (n_rows, n_columns) = self.dimensions
+        return Metadata(name=self.name,
+                        n_rows=n_rows,
+                        n_columns=n_columns)
+
+
+
+class StateType(Enum):
+    """The types of state of a Parser."""
+    NEW_BLOCK = 0
+    PARSED_DESCRIPTION = 1
+    PARSED_NAME = 2
+    PARSING_POINTS = 3
+
+
+class Parser:
+    """Parser provides the functionality to parse a polyfile line by line."""
+    def __init__(self, has_z_value: bool=False) -> None:
+        """[summary]
+
+        Args:
+            has_z_value (bool, optional): [description]. Defaults to False.
+        """
+        self._has_z_value = has_z_value
+
+        self._line = 0
+        self._new_block()
+
+        self._poly_objects: List[PolyObject] = []
+        self._warnings: List[ParseMsg] = []
+        self._errors: List[ParseMsg] = []
+
+        self._current_point: int = 0
+        
+        self._feed_line: Dict[StateType, Callable[[str], None]] = { 
+            StateType.NEW_BLOCK          : self._parse_name_or_new_description,
+            StateType.PARSED_DESCRIPTION : self._parse_name_or_next_description,
+            StateType.PARSED_NAME        : self._parse_dimensions,
+            StateType.PARSING_POINTS     : self._parse_next_point,
+        }
+
+        self._finalise: Dict[StateType, Callable[[], None]] = { 
+            StateType.NEW_BLOCK          : self._noop,
+            StateType.PARSED_DESCRIPTION : self._add_current_block_as_incomplete_error,
+            StateType.PARSED_NAME        : self._add_current_block_as_incomplete_error,
+            StateType.PARSING_POINTS     : self._add_current_block_as_incomplete_error,
+        }
+
+    def feed_line(self, line: str) -> None:
+        """Parse the next line with this Parser.
+
+        Args:
+            line (str): The line to parse
+        """
+        self._feed_line[self._state](line)
+        self._increment_line()
+
+    def finalise(self) -> Tuple[Sequence[PolyObject], 
+                                Sequence[ParseMsg], 
+                                Sequence[ParseMsg]]:
+        """Finalise parsing and return the constructed objects.
+
+        Returns:
+            Tuple[Sequence[PolyObject], Sequence[ParseMsg], Sequence[ParseMsg]]:
+                A tuple consisting of the constructed PolyObject instances, error 
+                ParseMsg objects and warning ParseMsg objects.
+        """
+        self._finalise[self._state]
+        return self._poly_objects, self._errors, self._warnings
+
+    @property
+    def line(self) -> int:
+        """Current line index."""
+        return self._line
+
+    def _new_block(self) -> None:
+        self._state = StateType.NEW_BLOCK
+        self._current_block = Block(start_line=self.line)
+
+    def _finish_block(self):
+        self._poly_objects.append(PolyObject(
+                description = self._current_block.get_description(),
+                metadata= self._current_block.get_metadata(),  # type: ignore
+                points=self._current_block.points))
+        # handle error here
+
+    def _increment_line(self) -> None:
+        self._line += 1
+
+    def _noop(self) -> None:
+        # no operation
+        pass
+
+    def _add_current_block_as_incomplete_error(self) -> None:
+        self._errors.append(ParseMsg(line=(self._current_block.start_line, self.line), 
+                                     reason="EoF encountered before the block is finished."))
+
+    def _parse_name_or_new_description(self, line: str) -> None:
+        if Parser._is_comment(line):
+            self._handle_new_description(line)
+        elif Parser._is_name(line):
+            self._handle_parse_name(line)
+        else:
+            # Handle exception here
+            pass
+
+    def _parse_name_or_next_description(self, line: str) -> None:
+        if Parser._is_comment(line):
+            self._handle_next_description(line)
+        elif Parser._is_name(line):
+            self._handle_parse_name(line)
+        else:
+            # Handle exception here
+            pass
+
+    def _parse_dimensions(self, line: str) -> None:
+        dimensions = Parser._convert_to_dimensions(line)
+
+        if dimensions is not None:
+            self._current_block.dimensions = dimensions
+            self._current_block.points = []
+            self._current_point = 0
+            self._state = StateType.PARSING_POINTS
+        else:
+            # Handle exception here
+            pass
+
+    def _parse_next_point(self, line: str) -> None:
+        point = Parser._convert_to_point(
+            line, 
+            self._current_block.dimensions[1], # type: ignore
+            self._has_z_value)
+
+        if point is not None:
+            self._current_block.points.append(point) # type: ignore
+            self._current_point += 1
+
+            if self._current_block.dimensions[0] == self._current_point: # type: ignore
+                self._finish_block()
+                self._new_block()
+
+        else:
+            # Handle exception here
+            pass
+
+    @staticmethod
+    def _parse_point(line) -> Tuple[int, int]: 
+        return (-1, -1)
+
+    def _handle_parse_name(self, line: str) -> None:
+        self._current_block.name = Parser._convert_to_name(line)
+        self._state = StateType.PARSED_NAME
+
+    def _handle_new_description(self, line: str) -> None:
+        comment = Parser._convert_to_comment(line)
+        self._current_block.description = [ comment, ]
+        self._state = StateType.PARSED_DESCRIPTION
+
+    def _handle_next_description(self, line: str) -> None:
+        comment = Parser._convert_to_comment(line)
+        self._current_block.description.append(comment) # type: ignore
+
+    @staticmethod
+    def _is_name(line: str) -> bool:
+        stripped = line.strip()
+        return len(stripped) >= 1 and line[0] != '*' and not (' ' in stripped)
+
+    @staticmethod 
+    def _convert_to_name(line: str) -> str:
+        return line.strip()
+
+    @staticmethod
+    def _is_comment(line: str) -> bool:
+        return len(line) >= 1 and line[0] == '*'
+
+    @staticmethod
+    def _convert_to_comment(line: str) -> str:
+        return line.rstrip()[1:]
+
+    @staticmethod
+    def _convert_to_dimensions(line: str) -> Optional[Tuple[int, int]]:
+        stripped = line.strip()
+        elems = stripped.split()
+
+        if len(elems) != 2:
+            return None
+
+        try:
+            n_rows = int(elems[0])
+            n_cols = int(elems[1])
+
+            if n_rows <= 0 or n_cols <= 0:
+                return None
+
+            return (n_rows, n_cols)
+        except ValueError:
+            return None
+
+    @staticmethod
+    def _convert_to_point(line: str, expected_n_points: int, has_z: bool) -> Optional[Point]:
+        stripped = line.strip()
+        elems = stripped.split()
+
+        if len(elems) != expected_n_points:
+            return None
+
+        try:
+            values = list(float(x) for x in elems)
+            return Point(
+                x = values[0],
+                y = values[1],
+                z = values[2] if has_z else None,
+                data=values[(3 if has_z else 2 ):])
+
+        except ValueError:
+            return None
---
+++ tests/io/test_polyfile.py
@@ -0,0 +1,104 @@
+from hydrolib.io.polyfile import Parser, Point
+from typing import Optional, Tuple
+
+import pytest
+
+
+class TestParser:
+    @pytest.mark.parametrize(
+        "input,expected_value", 
+        [
+            ("* some comment", True),
+            ("*", True),
+            ("*         ", True),
+            ("", False),
+            ("potato", False),
+            ("1_1", False),
+            ("1_1:type=terrainjump", False),
+            ("Not a comment but a regular string", False),
+            ("      ", False),
+            ("         * some comment", False),
+        ])
+    def test_is_comment(self, input: str, expected_value: bool):
+        assert Parser._is_comment(input) == expected_value
+
+    @pytest.mark.parametrize(
+        "input,expected_value",
+        [
+            ("* some comment", " some comment"),
+            ("*", ""),
+            ("*   ", ""),
+        ]
+    )
+    def test_convert_to_comment(self, input: str, expected_value: str):
+        assert Parser._convert_to_comment(input) == expected_value
+
+    @pytest.mark.parametrize(
+        "input,expected_value", 
+        [
+            ("potato", True),
+            ("1_1", True),
+            ("1_1:type=terrainjump", True),
+            ("* some comment", False),
+            ("*", False),
+            ("*         ", False),
+            ("", False),
+            ("Not a comment but a regular string", False),
+            ("      ", False),
+        ])
+    def test_is_name(self, input: str, expected_value: bool):
+        assert Parser._is_name(input) == expected_value
+
+    @pytest.mark.parametrize(
+        "input,expected_value", 
+        [
+            ("potato", "potato"),
+            ("    potato     ", "potato"),
+            ("1_1  ", "1_1"),
+            ("1_1:type=terrainjump", "1_1:type=terrainjump"),
+        ])
+    def test_convert_to_name(self, input: str, expected_value: str):
+        assert Parser._convert_to_name(input) == expected_value
+
+
+    @pytest.mark.parametrize(
+        "input,expected_value", 
+        [
+            ("1 1", (1, 1)),
+            ("   201     88    ", (201, 88)),
+            ("0 1", None),
+            ("201 0", None),
+            ("0 0", None),
+            ("-5 20", None),
+            ("-5 -20", None),
+            ("5 -20", None),
+            ("1 1.0", None),
+            ("1.0 1", None),
+            ("1.0 1.0", None),
+        ])
+    def test_convert_to_dimensions(self, input: str, expected_value: Optional[Tuple[int, int]]):
+        assert Parser._convert_to_dimensions(input) == expected_value
+
+    @pytest.mark.parametrize(
+        "input,n_total_points,has_z,expected_value", 
+        [
+            ("1.0  2.0", 2, False, Point(x=1.0, y=2.0, z=None, data=[])),
+            ("1.0  2.0  3.0", 3, True, Point(x=1.0, y=2.0, z=3.0, data=[])),
+            ("1.0  2.0  3.0", 3, False, Point(x=1.0, y=2.0, z=None, data=[3.0,])),
+            ("1.0  2.0  3.0  4.0  5.0", 5, True, Point(x=1.0, y=2.0, z=3.0, data=[4.0, 5.0,])),
+            ("    1.0    2.0    3.0    4.0    5.0        ", 5, True, Point(x=1.0, y=2.0, z=3.0, data=[4.0, 5.0,])),
+            ("    1    2    3    4    5        ", 5, True, Point(x=1.0, y=2.0, z=3.0, data=[4.0, 5.0,])),
+            ("    a    2    3    4    5        ", 5, True, None),
+            ("    1.0    2.0    3.0    4.0    5.0        ", 3, True, None),
+            ("    1.0    2.0    3.0    4.0    5.0        ", 6, True, None),
+            ("1.0,  2.0,", 2, False, None),
+            ("1.a  2.b", 2, False, None),
+        ]
+    )
+    def test_convert_to_point(
+            self, 
+            input: str, 
+            n_total_points: int, 
+            has_z: bool, 
+            expected_value: Point):
+        assert Parser._convert_to_point(input, n_total_points, has_z) == expected_value
