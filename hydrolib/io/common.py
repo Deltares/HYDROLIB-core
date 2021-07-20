d52a4467d757a860eb0f44f05227f3faeae685d6
tegelaer <maarten.tegelaers@deltares.nl>
2021-07-20 07:35:52 +0200

    Adjust ParseMsg and make error level implicit

--- hydrolib/io/common.py
+++ hydrolib/io/common.py
@@ -3,7 +3,7 @@
 
 from enum import Enum
 from pydantic import BaseModel as PydanticBaseModel
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 # TODO: Replace this BaseModel with the basemodel defined in basemodel.py once in main
@@ -15,27 +15,9 @@ class BaseModel(PydanticBaseModel):
         extra = "allow"
 
 
-class ParseErrorLevel(Enum):
-    """ParseErrorLevel defines the possible error levels of ParseMsg objects.
-
-    - FATAL indicates we could not recover from the error, and no result will be
-      returned.
-    - ERROR indicates the integrity of the data is compromised, however we can
-      continue to read data.
-    - WARNING indicates an unexpected event, however the integrity of the data
-      is ensured.
-    - INFO indicates some event that does not necessarily require user intervention.
-    """
-    FATAL = 0
-    ERROR = 1
-    WARNING = 2
-    INFO = 3
-
-
 class ParseMsg(BaseModel):
     """ParseMsg defines a single message indicating a significant parse event.
     """
-    level: ParseErrorLevel
     line: Tuple[int, int]
-    column: Tuple[int, int]
+    column: Optional[Tuple[int, int]]
     reason: str
