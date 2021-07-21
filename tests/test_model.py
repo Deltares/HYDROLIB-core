72540275e919e9c323c232ab25909d3a1053fe35
Maarten Pronk <git@evetion.nl>
2021-07-21 21:09:49 +0200

    Rewrote tests.

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,27 +2,29 @@ from pathlib import Path
 
 import pytest
 
-from hydrolib.core.basemodel import FileModel
-from hydrolib.core.models import DIMR, FMModel, XYZ
+from hydrolib.core.io.dimr.models import FMComponent, RRComponent
+from hydrolib.core.models import DIMR, XYZ, FMModel, Network
 
 from .utils import test_data_dir, test_output_dir
 
 
-def test_filemodel_load():
+def test_filemodel_recursive_load():
     # If we give a non existing path, it will throw a warning
-    with pytest.warns(UserWarning):
-        d = DIMR(filepath="a", model=Path(test_data_dir / "test.mdu"))
+    d = FMModel(filepath=Path(test_data_dir / "test.mdu"))
     assert isinstance(d.filepath, Path)
-    assert isinstance(d.model, FMModel)
-    assert isinstance(d.model, FileModel)
+    assert isinstance(d.network, Network)
 
 
-def test_filemodel_serialize():
-    d = DIMR(filepath=f"{test_output_dir}/a", model=Path(test_data_dir / "test.mdu"))
+def test_filemodel_recursive_serialize():
+    d = FMModel(filepath=Path(test_data_dir / "test.mdu"))
     d.save(folder=test_output_dir / "tmp")
     assert d.filepath.is_file()
-    assert d.model.filepath.is_file()
-    assert d.model.network.filepath.is_file()
+    assert d.network.filepath.is_file()
+
+
+def test_warn_on_non_existing_path():
+    with pytest.warns(UserWarning):
+        FMModel(filepath=Path("a"))
 
 
 def test_dimr_model():
@@ -34,23 +36,31 @@ def test_dimr_model():
         / "dimr_model"
         / "dimr_config.xml"
     )
+    # Confirm parsing results in correct
+    # components for each type of submodel
     d = DIMR(filepath=test_file)
+    assert len(d.component) == 2
+    assert isinstance(d.component[0], RRComponent)
+    assert isinstance(d.component[1], FMComponent)
+
+    # Confirm saving creates new files and
+    # files for child model
     d.save(folder=test_output_dir / "tmp")
     assert d.filepath.is_file()
     assert d.component[1].model.filepath.is_file()
 
 
-def test_tutorial():
-    model = FMModel(name="testproject")
-    model.save(folder=test_output_dir / "test")
-    assert model.filepath.is_file()
-    assert model.network.filepath.is_file()
-
-
 def test_xyz_model():
+    output_fn = Path(test_output_dir / "test.xyz")
+    if output_fn.is_file():
+        output_fn.unlink()
+
+    # Confirm succesfull parse and initialization
     model = XYZ(filepath=Path(test_data_dir / "input/test.xyz"))
     assert len(model.points) == 7, model
-    model.filepath = Path(test_output_dir / "test.xyz")
+
+    # Confirm saving to new file
+    model.filepath = output_fn
     assert not model.filepath.is_file()
     model.save()
     assert model.filepath.is_file()
