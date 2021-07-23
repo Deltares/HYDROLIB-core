199621c1fd8521d95371a85efebf3261282306b3
tegelaer <maarten.tegelaers@deltares.nl>
2021-07-23 21:29:04 +0200

    Restructure code and add documentation to io.ini

--- hydrolib/core/io/ini/models.py
+++ hydrolib/core/io/ini/models.py
@@ -1,27 +1,83 @@
-from typing import Any, List, Optional
-
 from hydrolib.core.basemodel import BaseModel
+from typing import List, Optional, Sequence, Union
 
 
-class IniProperty(BaseModel):
-    """Property from an INI file.
+class CommentBlock(BaseModel):
+    """CommentBlock defines a comment block within a deltares ini file.
 
     Attributes:
-        key: The key of the property
-        value: The value of the propery
-        comment: The comment of the property
+        start_line (int): The start line index of the CommentBlock within the file
+        end_line (int): The end line index of the CommentBlock within the file
+        lines (List[str]): The actual lines of the CommentBlock
     """
 
+    start_line: int
+    end_line: int
+    lines: List[str]
+
+
+class Property(BaseModel):
+    """Property defines a deltares ini property
+
+    Attributes:
+        line (int): The line index on which this Property is defined
+        key (str): The key of this Property
+        value (Optional[str]): The value associated with this Property
+        comment (Optional[str]): The comment associated with this Property
+    """
+
+    line: int
     key: str
-    value: Optional[Any]
+    value: Optional[str]
     comment: Optional[str]
 
 
-class IniSection(BaseModel):
-    """Section from an INI file.
+class Section(BaseModel):
+    """Section defines a deltares ini section
+
+    A deltares ini section consists of a header and an ordered list of
+    properties and comments.
+
+    For example:
+
+    ```
+    [header-title]
+    a=1
+    b=2
+    # and a comment
+    c=4
+    ```
+
+    An optional datablock can be defined (used in for example .bc files).
 
     Attributes:
-        properties: List of [`IniProperty`][hydrolib.core.io.ini.models.IniProperty] that are contained by this section.
+        header (str): The header (without brackets) of this Section
+        start_line (int): The starting line index of this Section block
+        end_line (int): The end line index of this Section block
+        content (List[Union[Property, CommentBlock]]):
+            The ordered list of Property and CommentBlock objects
+        datablock (Optional[Sequence[Sequence[str]]]):
+            An optional data block associated with this Section
     """
 
-    properties: List[IniProperty]
+    header: str
+    start_line: int
+    end_line: int
+    content: List[Union[Property, CommentBlock]]
+
+    # these are primarily relevant for bc files
+    datablock: Optional[Sequence[Sequence[str]]]
+
+
+class Document(BaseModel):
+    """Document defines a Deltares ini document
+
+    Attributes:
+        header_comment (List[CommentBlock]):
+            An ordered list of comment blocks defined in the header of the Document
+        sections (List[Section]):
+            An ordered list of sections defined in this Document
+    """
+
+    header_comment: List[CommentBlock] = []
+    sections: List[Section] = []
--- hydrolib/core/io/ini/parser.py
+++ hydrolib/core/io/ini/parser.py
@@ -1,105 +1,31 @@
 from enum import IntEnum
 from hydrolib.core.basemodel import BaseModel
+from hydrolib.core.io.ini.models import CommentBlock, Document, Property, Section
 from pydantic import validator
 from typing import Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 
-class StateType(IntEnum):
-    NO_SECTION_FOUND = 0
-    PARSING_PROPERTIES = 1
-    PARSING_DATABLOCK = 2
-
-
-# DataAccess objects
-class CommentBlock(BaseModel):
-    start_line: int
-    end_line: int
-    lines: List[str]
-
-
-class Property(BaseModel):
-    line: int
-    key: str
-    value: Optional[str]
-    comment: Optional[str]
-
-
-class Section(BaseModel):
-    header: str
-    start_line: int
-    end_line: int
-    content: List[Union[Property, CommentBlock]]
-
-    # these are primarily relevant for bc files
-    datablock: Optional[Sequence[Sequence[str]]]
-
-
-class IntermediateCommentBlock(BaseModel):
-    start_line: int
-    _lines: List[str] = []
-
-    def add_comment_line(self, line: str) -> None:
-        self._lines.append(line)
-
-    def finalize(self) -> CommentBlock:
-        return CommentBlock(
-            start_line=self.start_line,
-            end_line=self.start_line + len(self._lines) - 1,
-            lines=self._lines,
-        )
-
-
-# Intermediate objects
-class IntermediateSection(BaseModel):
-    header: str
-    start_line: int
-    _content: List[Union[Property, CommentBlock]] = []
-    _datablock: List[List[str]] = []
-    _curr_comment_block: Optional[IntermediateCommentBlock]
-
-    def add_property(self, property: Property) -> None:
-        self._finalize_comment_block()
-        self._content.append(property)
-
-    def add_comment(self, comment: str, line: int) -> None:
-        if self._curr_comment_block is None:
-            self._curr_comment_block = IntermediateCommentBlock(start_line=line)
-        self._curr_comment_block.add_comment_line(comment)
-
-    def add_datarow(self, row: List[str]) -> None:
-        self._datablock.append(row)
-
-    def _finalize_comment_block(self) -> None:
-        if self._curr_comment_block is None:
-            return
-
-        self._content.append(self._curr_comment_block.finalize())
-        self._curr_comment_block = None
-
-    def finalize(self, end_line) -> Section:
-        self._finalize_comment_block()
-        return Section(
-            header=self.header,
-            start_line=self.start_line,
-            end_line=end_line,
-            content=self._content,
-            datablock=self._datablock if self._datablock else None,
-        )
-
-
-class Document(BaseModel):
-    header_comment: List[CommentBlock] = []
-    sections: List[Section] = []
-
-
-class RuleSet(BaseModel):
-    allow_recurrent_section_names: bool = True
-    allow_recurrent_properties: bool = True
-    comment_character: str = "#"  # this should be moved to the parser
-
-
 class ParserConfig(BaseModel):
-    """ParserConfig defines the configuration options of the Parser"""
+    """ParserConfig defines the configuration options of the Parser
+
+    Note that we cannot set both allow_only_keywords and parse_datablocks to True
+    because we cannot distinguish between datablocks and key only properties. As
+    such this will lead to a validation error.
+
+    Attributes:
+        allow_only_keywords (bool):
+            Whether to allow properties with only keys (no '=' or value).
+            Defaults to True.
+        parse_datablocks (bool):
+            Whether to allow parsing of datablocks at the bottom of sections.
+            Defaults to False.
+        parse_comments (bool):
+            Whether we allow parsing of comments defined with the comment_delimeter.
+            Defaults to True.
+        comment_delimeter (str):
+            The character or sequence of character used to define a comment.
+            Defaults to '#'.
+    """
 
     allow_only_keywords: bool = True
     parse_datablocks: bool = False
@@ -123,44 +49,120 @@ class ParserConfig(BaseModel):
             )
 
 
+class _IntermediateCommentBlock(BaseModel):
+    # _IntermediateCommentBlock used to construct CommentBlock objects within
+    # this Parser.
+    start_line: int
+    _lines: List[str] = []
+
+    def add_comment_line(self, line: str) -> None:
+        self._lines.append(line)
+
+    def finalize(self) -> CommentBlock:
+        return CommentBlock(
+            start_line=self.start_line,
+            end_line=self.start_line + len(self._lines) - 1,
+            lines=self._lines,
+        )
+
+
+class _IntermediateSection(BaseModel):
+    # _IntermediateSection used to construct section objects within
+    # this Parser.
+    header: str
+    start_line: int
+    _content: List[Union[Property, CommentBlock]] = []
+    _datablock: List[List[str]] = []
+    _curr_comment_block: Optional[_IntermediateCommentBlock]
+
+    def add_property(self, property: Property) -> None:
+        self._finalize_comment_block()
+        self._content.append(property)
+
+    def add_comment(self, comment: str, line: int) -> None:
+        if self._curr_comment_block is None:
+            self._curr_comment_block = _IntermediateCommentBlock(start_line=line)
+        self._curr_comment_block.add_comment_line(comment)
+
+    def add_datarow(self, row: List[str]) -> None:
+        self._datablock.append(row)
+
+    def _finalize_comment_block(self) -> None:
+        if self._curr_comment_block is None:
+            return
+
+        self._content.append(self._curr_comment_block.finalize())
+        self._curr_comment_block = None
+
+    def finalize(self, end_line) -> Section:
+        self._finalize_comment_block()
+        return Section(
+            header=self.header,
+            start_line=self.start_line,
+            end_line=end_line,
+            content=self._content,
+            datablock=self._datablock if self._datablock else None,
+        )
+
+
 # TODO: generalise StateBasedLineParser, and let both Ini.Parser and Polyfile.Parser inherit
 class Parser:
+    """Parser defines a generic Parser for Deltares ini files.
+
+    The Parser can be configured with a ParserConfig object.
+    """
+
+    class _StateType(IntEnum):
+        NO_SECTION_FOUND = 0
+        PARSING_PROPERTIES = 1
+        PARSING_DATABLOCK = 2
+
     def __init__(self, config: ParserConfig) -> None:
+        """Creates a new Parser configured with the provided config
+
+        Args:
+            config (ParserConfig): The configuration of this Parser
+        """
         self._config = config
         self._document = Document()
-        self._current_section: Optional[IntermediateSection] = None
-        self._current_header_block: Optional[IntermediateCommentBlock] = None
+        self._current_section: Optional[_IntermediateSection] = None
+        self._current_header_block: Optional[_IntermediateCommentBlock] = None
 
-        self._state = StateType.NO_SECTION_FOUND
+        self._state = self._StateType.NO_SECTION_FOUND
         self._line_index = 0
 
         # TODO add invalid blocks
         self._feed_line: Dict[
-            StateType, List[Tuple[Callable[[str], bool], Callable[[str], None]]]
+            Parser._StateType, List[Tuple[Callable[[str], bool], Callable[[str], None]]]
         ] = {
-            StateType.NO_SECTION_FOUND: [
+            Parser._StateType.NO_SECTION_FOUND: [
                 (self._is_comment, self._handle_header_comment),
                 (self._is_section_header, self._handle_next_section_header),
             ],
-            StateType.PARSING_PROPERTIES: [
+            Parser._StateType.PARSING_PROPERTIES: [
                 (self._is_comment, self._handle_section_comment),
                 (self._is_section_header, self._handle_next_section_header),
                 (self._is_property, self._handle_property),
                 (self._is_datarow, self._handle_new_datarow),
             ],
-            StateType.PARSING_DATABLOCK: [
+            Parser._StateType.PARSING_DATABLOCK: [
                 (self._is_section_header, self._handle_next_section_header),
                 (self._is_datarow, self._handle_datarow),
             ],
         }
 
-        self._handle_emptyline: Dict[StateType, Callable[[], None]] = {
-            StateType.NO_SECTION_FOUND: self._handle_emptyline_header,
-            StateType.PARSING_PROPERTIES: self._noop,
-            StateType.PARSING_DATABLOCK: self._noop,
+        self._handle_emptyline: Dict[Parser._StateType, Callable[[], None]] = {
+            self._StateType.NO_SECTION_FOUND: self._handle_emptyline_header,
+            self._StateType.PARSING_PROPERTIES: self._noop,
+            self._StateType.PARSING_DATABLOCK: self._noop,
         }
 
     def feed_line(self, line: str) -> None:
+        """Parse the next line with this Parser.
+
+        Args:
+            line (str): The line to parse
+        """
         if not self._is_empty_line(line):
             for (is_line_type, handle_line_type) in self._feed_line[self._state]:
                 if is_line_type(line):
@@ -175,6 +177,12 @@ class Parser:
         self._increment_line()
 
     def finalize(self) -> Document:
+        """Finalize parsing and return the constructed Document.
+
+        Returns:
+            Document:
+                A Document describing the parsed ini file.
+        """
         # TODO handle invalid block
         self._finalise_current_section()
         return self._document
@@ -186,11 +194,11 @@ class Parser:
         self._finalise_current_section()
         self._handle_new_section_header(line)
 
-        self._state = StateType.PARSING_PROPERTIES
+        self._state = Parser._StateType.PARSING_PROPERTIES
 
     def _handle_new_section_header(self, line: str) -> None:
         section_header = line.strip()[1:-1].strip()
-        self._current_section = IntermediateSection(
+        self._current_section = _IntermediateSection(
             header=section_header, line=self._line_index
         )
 
@@ -202,7 +210,7 @@ class Parser:
 
     def _handle_header_comment(self, line: str) -> None:
         if self._current_header_block is None:
-            self._current_header_block = IntermediateCommentBlock(
+            self._current_header_block = _IntermediateCommentBlock(
                 start_line=self._line_index
             )
 
@@ -222,7 +230,7 @@ class Parser:
 
     def _handle_new_datarow(self, line: str) -> None:
         self._handle_datarow(line)
-        self._state = StateType.PARSING_DATABLOCK
+        self._state = Parser._StateType.PARSING_DATABLOCK
 
     def _handle_datarow(self, line: str) -> None:
         self._current_section.add_datarow(line.split(" "))  # type: ignore
