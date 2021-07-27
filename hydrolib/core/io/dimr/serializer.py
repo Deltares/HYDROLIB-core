5fc3a1f459209389b310639b2dcc3276649f0f14
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-07-27 08:53:34 +0200

    Add indentation to xml serialization

--- hydrolib/core/io/dimr/serializer.py
+++ hydrolib/core/io/dimr/serializer.py
@@ -21,30 +21,30 @@ class DIMRSerializer:
                 '<dimrConfig xmlns="http://schemas.deltares.nl/dimr" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.deltares.nl/dimr http://content.oss.deltares.nl/schemas/dimr-1.3.xsd">\n'
             )
 
-            DIMRSerializer._serialize_data(f, data)
+            DIMRSerializer._serialize_data(f, data, 2)
 
             f.write("</dimrConfig>\n")
 
     @staticmethod
-    def _serialize_data(f: TextIOWrapper, data: Dict):
+    def _serialize_data(f: TextIOWrapper, data: Dict, indent: int):
         for key in data:
             value = data[key]
             if isinstance(value, Dict):
-                DIMRSerializer._serialize_dict(f, key, value)
+                DIMRSerializer._serialize_dict(f, key, value, indent)
             elif isinstance(value, List):
                 for item in value:
-                    DIMRSerializer._serialize_dict(f, key, item)
+                    DIMRSerializer._serialize_dict(f, key, item, indent)
             else:
-                f.write(f"<{key}>{value}</{key}>\n")
+                f.write(indent * " " + f"<{key}>{value}</{key}>\n")
 
     @staticmethod
-    def _serialize_dict(f: TextIOWrapper, key: str, data: Dict):
+    def _serialize_dict(f: TextIOWrapper, key: str, data: Dict, indent: int):
         name = data.pop("name", None)
         if name:
-            f.write(f'<{key} name="{name}">\n')
+            f.write(indent * " " + f'<{key} name="{name}">\n')
         else:
-            f.write(f"<{key}>\n")
+            f.write(indent * " " + f"<{key}>\n")
 
-        DIMRSerializer._serialize_data(f, data)
+        DIMRSerializer._serialize_data(f, data, indent + 2)
 
-        f.write(f"</{key}>\n")
+        f.write(indent * " " + f"</{key}>\n")
