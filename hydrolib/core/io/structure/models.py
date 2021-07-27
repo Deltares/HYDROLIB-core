7ecb9b82b0406792ab5acd2938ff5b7548978728
tegelaer <maarten.tegelaers@deltares.nl>
2021-07-27 07:57:24 +0200

    Refactor structure/models.py to use aliases

--- hydrolib/core/io/structure/models.py
+++ hydrolib/core/io/structure/models.py
@@ -1,35 +1,20 @@
-from abc import ABC, abstractclassmethod, abstractclassmethod
+from abc import ABC
 from enum import Enum
 from hydrolib.core.io.ini.models import Property, Section
 from pathlib import Path
 from hydrolib.core.basemodel import BaseModel
-from pydantic import validator
-from pydantic.generics import GenericModel
-from pydantic.types import constr
-from typing import Any, Dict, Generic, List, Literal, Optional, Type, TypeVar, Union
-
-TPropertyField = TypeVar("TPropertyField")
-
-
-# This is more generic and should be moved to seperately
-class PropertyField(GenericModel, Generic[TPropertyField]):
-    key: str  # this really should be immutable
-    value: TPropertyField
-    comment: Optional[str] = None
-
-    @classmethod
-    def validate(cls: Type["PropertyField"], value: Any) -> "PropertyField":
-        if isinstance(value, Property):
-            value = value.dict(exclude={"line"})
-
-        return super().validate(value)
+from pydantic import validator, Field
+from pydantic.main import Extra
+from typing import Any, Dict, List, Literal, Optional, Type, Union
 
 
 # TODO: handle comment blocks
 # TODO: handle duplicate keys
 class IniBasedModel(BaseModel, ABC):
-    unknown_properties: List[PropertyField[Optional[str]]]
-    order: Optional[List[str]] = None
+    class Config:
+        extra = Extra.allow
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = False
 
     @classmethod
     def validate(cls: Type["IniBasedModel"], value: Any) -> "IniBasedModel":
@@ -39,11 +24,6 @@ class IniBasedModel(BaseModel, ABC):
             value = {**underlying_dict, **converted_content}
 
         return super().validate(value)
-
-    @classmethod
-    @abstractclassmethod
-    def _field_mapping(cls) -> Dict[str, str]:
-        return {}
 
     @classmethod
     def _convert_section_to_dict(cls, value: Section) -> Dict:
@@ -58,19 +38,8 @@ class IniBasedModel(BaseModel, ABC):
 
     @classmethod
     def _convert_section_content(cls, content: List):
-        data: Dict[str, Any] = {"unknown_properties": []}
-        mapping = cls._field_mapping()
-
-        for v in content:
-            if not isinstance(v, Property):
-                continue
-
-            if v.key in mapping:
-                data[mapping[v.key]] = v
-            else:
-                data["unknown_properties"].append(v)
-
-        return data
+        # TODO add comment here
+        return dict((v.key, v.value) for v in content if isinstance(v, Property))
 
 
 class DataBlockIniBasedModel(IniBasedModel):
@@ -86,69 +55,49 @@ class DataBlockIniBasedModel(IniBasedModel):
 
 
 class Structure(IniBasedModel):
-    id: PropertyField[constr(max_length=256)]  # type: ignore
-    name: PropertyField[str]
     header: Literal["Structure"] = "Structure"
 
-    # Either branchId and chainage are required or
-    # numCoordinates, xCoordinates, yCoordinates
-    # unless the Structure is a compound
-    # TODO: verify whether we can change this to model it better
-    # TODO: e.g. create a BranchLocation, LineLocation, and define a single location field as a Union[BranchLocation, LineLocation]
-    # TODO: Handle compound separately. Technically this seems to be a difference between 1D and 2D structures.
-    branch_id = PropertyField[Optional[str]](key="branchId", value=None)
+    id: str = Field("id", max_length=256)
+    name: str = Field("id")
 
-    chainage = PropertyField[Optional[float]](key="chainage", value=None)
+    # TODO: Note that either branch_id and chainage needs to be defined, or the coordinates. This shoud be either validated or refactored.
+    branch_id: Optional[str] = Field(None, alias="branchId")
+    chainage: Optional[float] = None
 
-    n_coordinates = PropertyField[Optional[int]](key="numCoordinates", value=None)
-
-    x_coordinates = PropertyField[Optional[List[float]]](key="xCoordinates", value=None)
-    y_coordinates = PropertyField[Optional[List[float]]](key="yCoordinates", value=None)
-
-    # TODO: Add validator for location
-    @classmethod
-    def _field_mapping(cls) -> Dict[str, str]:
-        # TODO: Can we derive these automatically?
-        structure_mappings = {
-            "id": "id",
-            "name": "name",
-            "branchId": "branch_id",
-            "chainage": "chainage",
-            "numCoordinates": "n_coordinates",
-            "xCoordinates": "x_coordinates",
-            "yCoordinates": "y_coordinates",
-        }
-        return {**super()._field_mapping(), **structure_mappings}
+    n_coordinates: Optional[int] = Field(None, alias="numCoordinates")
+    x_coordinates: Optional[List[float]] = Field(None, alias="xCoordinates")
+    y_coordinates: Optional[List[float]] = Field(None, alias="yCoordinates")
 
 
-class FlowDirection(Enum):
-    NONE = "none"
-    POSITIVE = "positive"
-    NEGATIVE = "negative"
-    BOTH = "both"
+class FlowDirection(str, Enum):
+    none = "none"
+    positive = "positive"
+    negative = "negative"
+    both = "both"
 
 
 class Weir(Structure):
-    structure_type = PropertyField[Literal["weir"]](key="type", value="weir")
+    structure_type: Literal["weir"] = Field("weir", alias="type")
+    allowed_flow_direction: FlowDirection = Field(alias="allowedFlowdir")
 
-    allowed_flow_direction = PropertyField[FlowDirection](
-        key="allowedFlowdir", value=FlowDirection.BOTH
-    )
+    crest_level: Union[float, Path] = Field(alias="crestLevel")
+    crest_width: Optional[float] = Field(None, alias="crestWidth")
+    correction_coefficient: float = Field(1.0, alias="corrCoeff")
+    use_velocity_height: bool = Field(True, alias="useVelocityHeight")
 
-    # TODO: verify default value
-    crest_level = PropertyField[Union[float, Path]](key="crestLevel", value=0.0)
-    crest_width = PropertyField[Optional[float]](key="crestWidth", value=None)
-    correction_coefficient = PropertyField[float](key="corrCoeff", value=1.0)
-    use_velocity_height = PropertyField[bool](key="useVelocityHeight", value=True)
 
-    @classmethod
-    def _field_mapping(cls) -> Dict[str, str]:
-        weir_mappings = {
-            "type": "structure_type",
-            "allowedFlowdir": "allowed_flow_direction",
-            "crestLevel": "crest_level",
-            "crestWidth": "crest_width",
-            "corrCoeff": "correction_coefficient",
-            "useVelocityHeight": "use_velocity_height",
-        }
-        return {**super()._field_mapping(), **weir_mappings}
+class UniversalWeir(Structure):
+    structure_type: Literal["universalWeir"] = Field("universalWeir", alias="type")
+    allowed_flow_direction: FlowDirection = Field(alias="allowedFlowdir")
+
+    number_of_levels: int = Field(alias="numLevels")
+    y_values: List[float] = Field(alias="yValues")
+    z_values: List[float] = Field(alias="zValues")
+    crest_level: float = Field(alias="crestLevel")
+    discharge_coefficient: float = Field(alias="dischargeCoeff")
+
+    @validator("y_values", "z_values", pre=True)
+    def split_value_lists(cls, v):
+        if isinstance(v, str):
+            v = v.split()
+        return v
--- tests/io/test_structure.py
+++ tests/io/test_structure.py
@@ -1,7 +1,7 @@
-from hydrolib.core.io.structure.models import FlowDirection, PropertyField, Weir
+from hydrolib.core.io.structure.models import FlowDirection, UniversalWeir, Weir
 from hydrolib.core.io.ini.parser import Parser, ParserConfig
 from pydantic.generics import GenericModel
-from typing import Generic, TypeVar
+from typing import Generic, List, TypeVar, Union
 
 import inspect
 
@@ -39,15 +39,15 @@ def test_weir_construction_with_parser():
     wrapper = TestWrapper[Weir].parse_obj({"val": document.sections[0]})
     weir = wrapper.val
 
-    assert weir.id.value == "weir_id"
-    assert weir.name.value == "weir"
-    assert weir.branch_id.value == "branch"
-    assert weir.chainage.value == 3.0
-    assert weir.structure_type.value == "weir"
-    assert weir.allowed_flow_direction.value == FlowDirection.POSITIVE
-    assert weir.crest_level.value == 10.5
-    assert weir.crest_width.value is None
-    assert weir.use_velocity_height.value == False
+    assert weir.id == "weir_id"
+    assert weir.name == "weir"
+    assert weir.branch_id == "branch"
+    assert weir.chainage == 3.0
+    assert weir.structure_type == "weir"
+    assert weir.allowed_flow_direction == FlowDirection.positive
+    assert weir.crest_level == 10.5
+    assert weir.crest_width is None
+    assert weir.use_velocity_height == False
 
 
 def test_weir_with_unknown_parameters():
@@ -81,20 +81,121 @@ def test_weir_with_unknown_parameters():
     wrapper = TestWrapper[Weir].parse_obj({"val": document.sections[0]})
     weir = wrapper.val
 
-    assert weir.unknown_properties == [
-        PropertyField(
-            key="unknown",
-            value="10.0",
-            comment=" A deliberately added unknown property",
-        )
+    assert weir.unknown == "10.0"  # type: ignore
+
+    assert weir.id == "weir_id"
+    assert weir.name == "weir"
+    assert weir.branch_id == "branch"
+    assert weir.chainage == 3.0
+    assert weir.structure_type == "weir"
+    assert weir.allowed_flow_direction == FlowDirection.positive
+    assert weir.crest_level == 10.5
+    assert weir.crest_width is None
+    assert weir.use_velocity_height == False
+
+
+def test_universal_construction_with_parser():
+    parser = Parser(ParserConfig())
+
+    input_str = inspect.cleandoc(
+        """
+        [Structure]
+        id                = uweir_id         # Unique structure id (max. 256 characters).
+        name              = W002             # Given name in the user interface.
+        branchId          = branch           # (optional) Branch on which the structure is located.
+        chainage          = 6.0              # (optional)   Chanage on the branch (m)
+        type              = universalWeir    # Structure type
+        allowedFlowdir    = positive         # Possible values: both, positive, negative, none.
+        numLevels         = 2                # Number of yz-Values.
+        yValues           = 1.0 2.0          # y-values of the cross section (m) 
+        zValues           = 3.0 4.0          # z-values of the cross section (m). (number of values = numLevels)
+        crestLevel        = 10.5             # Crest level of weir (m AD)
+        dischargeCoeff    = 0.5              # Discharge coefficient c_e (-)
+        """
+    )
+
+    for line in input_str.splitlines():
+        parser.feed_line(line)
+
+    document = parser.finalize()
+
+    wrapper = TestWrapper[UniversalWeir].parse_obj({"val": document.sections[0]})
+    universal_weir = wrapper.val
+
+    assert universal_weir.id == "uweir_id"
+    assert universal_weir.name == "W002"
+    assert universal_weir.branch_id == "branch"
+    assert universal_weir.chainage == 6.0
+    assert universal_weir.allowed_flow_direction == FlowDirection.positive
+    assert universal_weir.number_of_levels == 2
+    assert universal_weir.y_values == [1.0, 2.0]
+    assert universal_weir.z_values == [3.0, 4.0]
+    assert universal_weir.crest_level == 10.5
+    assert universal_weir.discharge_coefficient == 0.5
+
+
+def test_weir_and_universal_weir_resolve_from_parsed_document():
+    parser = Parser(ParserConfig())
+
+    input_str = inspect.cleandoc(
+        """
+        [Structure]
+        id                = weir_id     # Unique structure id (max. 256 characters).
+        name              = W001        # Given name in the user interface.
+        branchId          = branch      # (optional) Branch on which the structure is located.
+        chainage          = 3.0         # (optional)   Chanage on the branch (m)
+        type              = weir        # Structure type
+        allowedFlowdir    = positive    # Possible values: both, positive, negative, none.
+        crestLevel        = 10.5        # Crest level of weir (m AD)
+        crestWidth        =             # Width of weir (m)
+        useVelocityHeight = false       # Flag indicates whether the velocity height is to be calculated or not.
+
+        [Structure]
+        id                = uweir_id         # Unique structure id (max. 256 characters).
+        name              = W002             # Given name in the user interface.
+        branchId          = branch           # (optional) Branch on which the structure is located.
+        chainage          = 6.0              # (optional)   Chanage on the branch (m)
+        type              = universalWeir    # Structure type
+        allowedFlowdir    = positive         # Possible values: both, positive, negative, none.
+        numLevels         = 2                # Number of yz-Values.
+        yValues           = 1.0 2.0          # y-values of the cross section (m) 
+        zValues           = 3.0 4.0          # z-values of the cross section (m). (number of values = numLevels)
+        crestLevel        = 10.5             # Crest level of weir (m AD)
+        dischargeCoeff    = 0.5              # Discharge coefficient c_e (-)
+        """
+    )
+
+    for line in input_str.splitlines():
+        parser.feed_line(line)
+
+    document = parser.finalize()
+
+    wrapper = TestWrapper[List[Union[Weir, UniversalWeir]]].parse_obj(
+        {"val": document.sections}
+    )
+    expected_structures = [
+        Weir(
+            id="weir_id",
+            name="W001",
+            branch_id="branch",
+            chainage=3.0,
+            allowed_flow_direction=FlowDirection.positive,
+            crest_level=10.5,
+            crest_width=None,
+            use_velocity_height=False,
+        ),
+        UniversalWeir(
+            id="uweir_id",
+            name="W002",
+            branch_id="branch",
+            chainage=6.0,
+            allowed_flow_direction=FlowDirection.positive,
+            number_of_levels=2,
+            y_values=[1.0, 2.0],
+            z_values=[3.0, 4.0],
+            crest_level=10.5,
+            discharge_coefficient=0.5,
+        ),
     ]
 
-    assert weir.id.value == "weir_id"
-    assert weir.name.value == "weir"
-    assert weir.branch_id.value == "branch"
-    assert weir.chainage.value == 3.0
-    assert weir.structure_type.value == "weir"
-    assert weir.allowed_flow_direction.value == FlowDirection.POSITIVE
-    assert weir.crest_level.value == 10.5
-    assert weir.crest_width.value is None
-    assert weir.use_velocity_height.value == False
+    assert wrapper.val == expected_structures
