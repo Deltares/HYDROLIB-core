eb626ae98206d08cea80861506f3e0d99a25b1f8
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-07-27 10:30:53 +0200

    Refactor a little

--- hydrolib/core/io/dimr/serializer.py
+++ hydrolib/core/io/dimr/serializer.py
@@ -1,6 +1,6 @@
 from io import TextIOWrapper
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 
 class DIMRSerializer:
@@ -21,35 +21,37 @@ class DIMRSerializer:
                 '<dimrConfig xmlns="http://schemas.deltares.nl/dimr" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.deltares.nl/dimr http://content.oss.deltares.nl/schemas/dimr-1.3.xsd">\n'
             )
 
-            DIMRSerializer._serialize_data(f, data, 2)
+            for line in DIMRSerializer._serialize(data, 2):
+                # tuple with indentation level and text
+                f.write(line[0] * " " + line[1] + "\n")
 
             f.write("</dimrConfig>")
 
     @staticmethod
-    def _serialize_data(f: TextIOWrapper, data: Dict, indent: int):
+    def _serialize(data: Dict, indent: int):
         for key in data:
             value = data[key]
             if isinstance(value, Dict):
-                DIMRSerializer._serialize_dict(f, key, value, indent)
+                yield from DIMRSerializer._serialize_dict(key, value, indent)
             elif isinstance(value, List):
                 for item in value:
-                    DIMRSerializer._serialize_dict(f, key, item, indent)
+                    yield from DIMRSerializer._serialize_dict(key, item, indent)
             else:
-                f.write(indent * " " + f"<{key}>{value}</{key}>\n")
+                yield (indent, f"<{key}>{value}</{key}>")
 
     @staticmethod
-    def _serialize_dict(f: TextIOWrapper, key: str, data: Dict, indent: int):
+    def _serialize_dict(key: str, data: Dict, indent: int):
         name = data.pop("name", None)
 
         if name:
             if len(data) == 0:
-                f.write(indent * " " + f'<{key} name="{name}" />\n')
+                yield (indent, f'<{key} name="{name}" />')
                 return
 
-            f.write(indent * " " + f'<{key} name="{name}">\n')
+            yield (indent, f'<{key} name="{name}">')
         else:
-            f.write(indent * " " + f"<{key}>\n")
+            yield (indent, f"<{key}>")
 
-        DIMRSerializer._serialize_data(f, data, indent + 2)
+        yield from DIMRSerializer._serialize(data, indent + 2)
 
-        f.write(indent * " " + f"</{key}>\n")
+        yield (indent, f"</{key}>")
