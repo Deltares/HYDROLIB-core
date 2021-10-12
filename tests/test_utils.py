0274ea3543ca66a9aec01c19025e96dfe12cef71
Maarten Pronk <evetion@users.noreply.github.com>
2021-10-12 12:10:54 +0000

    Update tests/test_utils.py
    Co-authored-by: Prisca van der Sluis <36264671+priscavdsluis@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from hydrolib.core.io.mdu.models import Output
 
 
@@ -7,3 +9,9 @@ class TestSplitString:
         output = Output(statsinterval=string_with_multiple_floats)
 
         assert output.statsinterval == [1.0, 5.0]
+
+    def test_split_string_strip_semicolon(self):
+        string_with_multiple_files = "file1 ; file2"
+        output = Output(obsfile=string_with_multiple_files)
+
+        assert output.obsfile == [Path("file1"), Path("file2")]
