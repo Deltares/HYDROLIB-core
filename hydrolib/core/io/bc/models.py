e3bcc9ac019466c431f1a30d58b114f53feb1ddf
Arthur van Dam <arthurvd@gmail.com>
2021-10-21 08:21:56 +0200

    Get rid of unnecessary warnings about dropped comments in structure+bc files (#70)

--- hydrolib/core/io/bc/models.py
+++ hydrolib/core/io/bc/models.py
@@ -43,7 +43,7 @@ class ForcingBase(DataBlockINIBasedModel):
 
     @classmethod
     def _supports_comments(cls):
-        return False
+        return True
 
     @classmethod
     def _duplicate_keys_as_list(cls):
