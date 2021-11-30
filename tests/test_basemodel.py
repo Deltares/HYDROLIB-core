b34f5b85851eeef6edce86487bb4ddeb31d4f101
tegelaer <Maarten.Tegelaers@deltares.nl>
2021-11-30 13:03:54 +0100

    Add test to verify filemodel constructor behaviour
    - Ensure that FileModel objects created separately from the same
      filepath do not reference the same underlying memory

---
+++ tests/test_basemodel.py
@@ -0,0 +1,27 @@
+from hydrolib.core.basemodel import FileModel
+from hydrolib.core.io.dimr.parser import DIMRParser
+from hydrolib.core.io.dimr.models import DIMR
+
+from tests.utils import test_input_dir
+
+
+def test_loading_a_file_twice_returns_different_model_instances() -> None:
+    # If the same source file is read multiple times, we expect that
+    # multiple (deep) copies are returned, and not references to the
+    # same object.
+
+    # For the ease of testing, we use DIMR model, which implements FileModel
+    assert issubclass(DIMR, FileModel)
+
+    test_file = (
+        test_input_dir
+        / "e02"
+        / "c11_korte-woerden-1d"
+        / "dimr_model"
+        / "dimr_config.xml"
+    )
+
+    model_a = DIMR(test_file)
+    model_b = DIMR(test_file)
+
+    assert model_a is not model_b
