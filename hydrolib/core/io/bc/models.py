0451829b8d7083450d1e37aa14a4213b5f5320b6
Maarten Pronk <evetion@users.noreply.github.com>
2021-12-01 10:23:45 +0100

    Fixed all sonarcloud smells. (#139)
    * Fixed all smells.
    
    * autoformat: isort & black

--- hydrolib/core/io/bc/models.py
+++ hydrolib/core/io/bc/models.py
@@ -2,7 +2,7 @@ import logging
 from abc import ABC
 from enum import Enum
 from pathlib import Path
-from typing import Callable, Dict, List, Literal
+from typing import Callable, Dict, List, Literal, Optional
 
 from pydantic import Extra
 from pydantic.class_validators import validator
@@ -85,7 +85,7 @@ class ForcingBase(DataBlockINIBasedModel):
                 )
         return v
 
-    def _get_identifier(self, data: dict) -> str:
+    def _get_identifier(self, data: dict) -> Optional[str]:
         return data["name"] if "name" in data else None
 
     class Config:
--- hydrolib/core/io/net/writer.py
+++ hydrolib/core/io/net/writer.py
@@ -288,16 +288,6 @@ class UgridWriter:
         mesh1d_edge_offset.units = "m"
         mesh1d_edge_offset[:] = mesh1d.mesh1d_edge_branch_offset
 
-        # mesh1d_edge_x = ncfile.createVariable("mesh1d_edge_x", np.float64, "mesh1d_nEdges")
-        # mesh1d_edge_x.long_name = "x-coordinate of mesh edges"
-        # mesh1d_edge_x.units = "m"
-        # mesh1d_edge_x[:] = mesh1d.mesh1d_edge_x
-
-        # mesh1d_edge_y = ncfile.createVariable("mesh1d_edge_y", np.float64, "mesh1d_nEdges")
-        # mesh1d_edge_y.long_name = "y-coordinate of mesh edges"
-        # mesh1d_edge_y.units = "m"
-        # mesh1d_edge_y[:] = mesh1d.mesh1d_edge_y
-
         mesh1d_node_branch = ncfile.createVariable(
             "mesh1d_node_branch", "i4", "mesh1d_nNodes"
         )
@@ -328,7 +318,6 @@ class UgridWriter:
         nc_mesh2d.face_node_connectivity = "mesh2d_face_nodes"
         nc_mesh2d.max_face_nodes_dimension = "max_nmesh2d_face_nodes"
         nc_mesh2d.face_dimension = "mesh2d_nFaces"
-        # mesh2d.edge_face_connectivity = "mesh2d_edge_faces"
         nc_mesh2d.face_coordinates = "mesh2d_face_x mesh2d_face_y"
 
         # Nodes:
@@ -358,20 +347,6 @@ class UgridWriter:
         mesh2d_node_x[:] = mesh2d.mesh2d_node_x
         mesh2d_node_y[:] = mesh2d.mesh2d_node_y
 
-        # Edges:
-        # mesh2d_xu = ncfile.createVariable("mesh2d_edge_x", np.float64,  "nmesh2d_edges")
-        # mesh2d_yu = ncfile.createVariable("mesh2d_edge_y", np.float64,  "nmesh2d_edges")
-        # mesh2d_xu[:] = mesh2d_edgex")
-        # mesh2d_yu[:] = mesh2d_edgey")
-        # mesh2d_xu.long_name = 'x-coordinate of mesh edges'
-        # mesh2d_yu.long_name = 'y-coordinate of mesh edges'
-
-        # for var, dim in zip([mesh2d_xu, mesh2d_yu],  list('xy')):
-        #     setattr(var, 'units', 'm')
-        #     setattr(var, 'mesh', 'mesh2d')
-        #     setattr(var, 'location', 'edge')
-        #     setattr(var, 'standard_name', f'projection_{dim}_coordinate')
-
         mesh2d_en = ncfile.createVariable(
             "mesh2d_edge_nodes",
             "i4",
@@ -384,15 +359,6 @@ class UgridWriter:
         mesh2d_en.location = "edge"
         mesh2d_en.mesh = "mesh2d"
         mesh2d_en[:] = mesh2d.mesh2d_edge_nodes + mesh2d_en.start_index
-
-        # mesh2d_et = ncfile.createVariable("mesh2d_edge_types", "i4", mesh2d.edge_dimension)
-        # mesh2d_et.long_name = 'edge type (relation between edge and flow geometry)'
-        # mesh2d_et.coordinates = 'mesh2d_edge_x mesh2d_edge_y'
-        # mesh2d_et.location = 'edge'
-        # mesh2d_et.mesh = 'mesh2d'
-        # mesh2d_et.standard_name = ''
-        # mesh2d_et.units = ''
-        # mesh2d_et[:] = 2
 
         mesh2d_fn = ncfile.createVariable(
             "mesh2d_face_nodes",
