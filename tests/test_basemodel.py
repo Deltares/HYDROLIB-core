185a79511a894c08ad1690e4d2d602042ebf76d9
tegelaer <Maarten.Tegelaers@deltares.nl>
2021-12-03 12:52:47 +0100

    Add tests for FileLoadingContext aggregates
    Add tests for the following classes:
    - file_load_context
    - FilePathResolver
    - FileModelCache

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -1,4 +1,17 @@
-from hydrolib.core.basemodel import FileModel
+from pathlib import Path
+from typing import Sequence, Tuple
+
+import pytest
+
+from hydrolib.core.basemodel import (
+    FileLoadContext,
+    FileModel,
+    FileModelCache,
+    FilePathResolver,
+    ResolveRelativeMode,
+    context_file_loading,
+    file_load_context,
+)
 from hydrolib.core.io.dimr.models import DIMR
 from tests.utils import test_input_dir
 
@@ -23,3 +36,194 @@ def test_loading_a_file_twice_returns_different_model_instances() -> None:
     model_b = DIMR(test_file)
 
     assert model_a is not model_b
+
+
+class TestFileLoadContext:
+    def test_file_load_context_is_created_and_disposed_properly(self):
+        assert context_file_loading.get(None) is None
+
+        with file_load_context() as flc:
+            assert isinstance(flc, FileLoadContext)
+            assert context_file_loading.get(None) is flc
+
+        assert context_file_loading.get(None) is None
+
+    def test_file_load_context_called_multiple_times_provides_same_instance(self):
+        with file_load_context() as flc_root:
+            with file_load_context() as flc_child:
+                assert flc_child is flc_root
+
+    def test_file_model_cache_without_state_returns_none(self):
+        cache = FileModelCache()
+        assert cache.retrieve_model(Path("some/path")) is None
+
+    def test_file_model_cache_with_state_returns_correct_result(self):
+        cache = FileModelCache()
+
+        path = Path.cwd() / "some-dimr.xml"
+        model = DIMR()  # empty subclass of the FileModel
+
+        cache.register_model(path, model)
+
+        assert cache.retrieve_model(path) is model
+
+    @pytest.mark.parametrize(
+        ("parents", "n_to_pop", "input_path", "expected_result"),
+        [
+            pytest.param(
+                [],
+                0,
+                Path("somePath.xml"),
+                Path.cwd() / "somePath.xml",
+                id="without_parents_path_returns_relative_to_the_cwd",
+            ),
+            pytest.param(
+                [],
+                0,
+                test_input_dir / "somePath.xml",
+                test_input_dir / "somePath.xml",
+                id="absolute_path_returns_itself",
+            ),
+            pytest.param(
+                [],
+                5,
+                Path("somePath.xml"),
+                Path.cwd() / "somePath.xml",
+                id="without_parents_with_pop_last_parents_returns_relative_to_the_cwd",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                ],
+                1,
+                Path("somePath.xml"),
+                Path.cwd() / "somePath.xml",
+                id="with_pop_only_parent_path_returns_relative_to_the_cwd",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToParent),
+                ],
+                2,
+                Path("somePath.xml"),
+                test_input_dir / "1" / "somePath.xml",
+                id="with_pop_last_parents_path_returns_relative_",
+            ),
+            pytest.param(
+                [(test_input_dir / "otherPath", ResolveRelativeMode.ToParent)],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "otherPath" / "somePath.xml",
+                id="with_parent_path_returns_relative_to_parent",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "5" / "somePath.xml",
+                id="with_relative_parent_paths_returns_relative_to_last_parent",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToAnchor),
+                ],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "5" / "somePath.xml",
+                id="with_anchor_last_returns_relative_to_anchor",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "1" / "somePath.xml",
+                id="with_anchor_first_returns_relative_to_anchor",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "3" / "somePath.xml",
+                id="with_anchors_middle_returns_relative_to_last_anchor",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                0,
+                Path("somePath.xml"),
+                test_input_dir / "3" / "somePath.xml",
+                id="with_multiple_anchors_returns_relative_to_last_anchor",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                3,
+                Path("somePath.xml"),
+                test_input_dir / "2" / "somePath.xml",
+                id="with_pop_anchor_returns_relative_to_parent_before_anchor",
+            ),
+            pytest.param(
+                [
+                    (test_input_dir / "1", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "2", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "3", ResolveRelativeMode.ToAnchor),
+                    (test_input_dir / "4", ResolveRelativeMode.ToParent),
+                    (test_input_dir / "5", ResolveRelativeMode.ToParent),
+                ],
+                3,
+                Path("somePath.xml"),
+                test_input_dir / "1" / "somePath.xml",
+                id="with_pop_last_anchor_multiple_anchors_returns_relative_to_previous_anchor",
+            ),
+        ],
+    )
+    def test_path_resolver_resolves_to_expected_result(
+        self,
+        parents: Sequence[Tuple[Path, ResolveRelativeMode]],
+        n_to_pop: int,
+        input_path: Path,
+        expected_result: Path,
+    ):
+        resolver = FilePathResolver()
+
+        for path, mode in parents:
+            resolver.push_new_parent(path, mode)
+
+        for _ in range(n_to_pop):
+            resolver.pop_last_parent()
+
+        assert resolver.resolve(input_path) == expected_result
