66a383efc0c6792c5b182afdb75d66ba1d3ea1d5
arthurvd <arthurvd@users.noreply.github.com>
2021-12-16 19:58:18 +0000

    autoformat: isort & black

--- docs/include/mkdocs-macros/main.py
+++ docs/include/mkdocs-macros/main.py
@@ -1,13 +1,17 @@
 def define_env(env):
     "Hook function"
 
-    dflowfm_um_url = "https://content.oss.deltares.nl/delft3d/manuals/D-Flow_FM_User_Manual_1D2D.pdf"
-    sobek_um_url = "https://content.oss.deltares.nl/delft3d/manuals/SOBEK_User_Manual.pdf"
+    dflowfm_um_url = (
+        "https://content.oss.deltares.nl/delft3d/manuals/D-Flow_FM_User_Manual_1D2D.pdf"
+    )
+    sobek_um_url = (
+        "https://content.oss.deltares.nl/delft3d/manuals/SOBEK_User_Manual.pdf"
+    )
     hydrolib_core_url = "https://github.com/Deltares/HYDROLIB-core"
 
     @env.macro
     def mymacro():
-      return "Hello World"
+        return "Hello World"
 
     def _get_url(url: str, anchor: str = None):
         return url + ("#" + anchor if anchor else "")
@@ -15,30 +19,45 @@ def define_env(env):
     @env.macro
     def dflowfm_um(anchor: str = "", linktext: str = "D-Flow FM UM"):
         """Create Markdown link for D-Flow FM User Manual, with optional anchor/bookmark."""
-    
+
         return "[" + linktext + "](" + _get_url(dflowfm_um_url, anchor) + ")"
 
     @env.macro
     def sobek_um(anchor: str = "", linktext: str = "SOBEK UM"):
         """Create Markdown link for SOBEK User Manual, with optional anchor/bookmark."""
-    
+
         return "[" + linktext + "](" + _get_url(sobek_um_url, anchor) + ")"
 
     @env.macro
     def sobek_um(anchor: str = "", linktext: str = "SOBEK UM"):
         """Create Markdown link for SOBEK User Manual, with optional anchor/bookmark."""
-    
+
         return "[" + linktext + "](" + _get_url(sobek_um_url, anchor) + ")"
 
     @env.macro
     def gh_issue(number: int, linktext: str = None):
         """Create Markdown link to GitHub issue page."""
 
-        return "[" + (linktext or "#"+str(number)) + "](" + hydrolib_core_url + "/issues/" + str(number) + ")"
+        return (
+            "["
+            + (linktext or "#" + str(number))
+            + "]("
+            + hydrolib_core_url
+            + "/issues/"
+            + str(number)
+            + ")"
+        )
 
     @env.macro
     def gh_PR(number: int, linktext: str = None):
         """Create Markdown link to GitHub pull request page."""
 
-        return "[" + (linktext or "#"+str(number)) + "](" + hydrolib_core_url + "/pull/" + str(number) + ")"
-
+        return (
+            "["
+            + (linktext or "#" + str(number))
+            + "]("
+            + hydrolib_core_url
+            + "/pull/"
+            + str(number)
+            + ")"
+        )
