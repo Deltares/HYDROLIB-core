334645a738a086362a2954c982761a6043e92623
tegelaer <maarten.tegelaers@deltares.nl>
2022-06-27 20:05:21 +0200

    Add runtime.txt

--- binder/generate_requirements.py
+++ binder/generate_requirements.py
@@ -7,9 +7,14 @@ import toml
 
 # Based on: https://github.com/jla524/requirements/blob/main/src/convert.py
 def retrieve_packages(toml_content: Dict[str, Any]) -> Dict[str, str]:
-    packages = toml_content["tool"]["poetry"]["dependencies"]
+    packages = dict(toml_content["tool"]["poetry"]["dependencies"])
     packages.pop("python", None)
     return packages
+
+
+def retrieve_python_version(toml_content: Dict[str, Any]) -> str:
+    version = toml_content["tool"]["poetry"]["dependencies"]["python"].strip("~^")
+    return f"python-{version}"
 
 
 def to_package(name: str, version: str) -> str:
@@ -31,24 +36,28 @@ def generate_requirements_content(packages: Dict[str, str]) -> str:
     return header + body
 
 
-def generate(src_path: Path, target_path: Path) -> None:
-    with src_path.open("r") as f:
+def generate(toml_path: Path, requirements_path: Path, runtime_path: Path) -> None:
+    with toml_path.open("r") as f:
         content = toml.load(f)
 
     packages = retrieve_packages(content)
     requirements = generate_requirements_content(packages)
 
-    with target_path.open("w") as f:
+    with requirements_path.open("w") as f:
         f.write(requirements)
+
+    python_version = retrieve_python_version(content)
+
+    with runtime_path.open("w") as f:
+        f.write(python_version)
 
 
 def parse_args():
     """Parses and returns the arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("toml_path", help="Path to the toml source file.")
-    parser.add_argument(
-        "requirements_path", help="Path to the target requirements.txt file."
-    )
+    parser.add_argument("requirements_path", help="Path to the requirements.txt file.")
+    parser.add_argument("runtime_path", help="Path to the runtime.txt file.")
 
     return parser.parse_args()
 
@@ -57,5 +66,6 @@ if __name__ == "__main__":
     args = parse_args()
     toml_path = Path(args.toml_path)
     requirements_path = Path(args.requirements_path)
+    runtime_path = Path(args.runtime_path)
 
-    generate(toml_path, requirements_path)
+    generate(toml_path, requirements_path, runtime_path)
