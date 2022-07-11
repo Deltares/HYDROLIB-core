ae7255e5c05c327800bbde0abdbee1044655917c
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2022-07-11 14:24:24 +0200

    Initial setup for supporting the new Observation CrossSection file.

---
+++ hydrolib/core/io/obscrosssection/models.py
@@ -0,0 +1,22 @@
+from typing import List, Literal
+from pydantic.fields import Field
+
+from hydrolib.core.io.ini.models import INIBasedModel, INIGeneral, INIModel
+
+
+class ObservationPointCrossSectionGeneral(INIGeneral):
+    filetype: Literal["obsCross"] = Field("obsCross", alias="fileType")
+
+
+class ObservationPointCrossSection(INIBasedModel):
+    pass
+
+
+class ObservationPointCrossSectionModel(INIModel):
+    """
+    The overall observation point crosssection model that contains the contents
+    of one observation point crosssection file.
+    """
+
+    general: ObservationPointCrossSectionGeneral = ObservationPointCrossSectionGeneral()
+    crosssections: List[ObservationPointCrossSection] = []
---
+++ tests/io/test_obscrosssection.py
@@ -0,0 +1,12 @@
+import pytest
+
+from hydrolib.core.io.obscrosssection.models import (
+    ObservationPointCrossSectionModel,
+    ObservationPointCrossSectionGeneral,
+    ObservationPointCrossSection
+)
+
+
+class TestObservationPointCrossSection:
+    def test_create(self):
+        pass
