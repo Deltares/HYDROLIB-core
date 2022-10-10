b4520433bd32bd63c13ac4e306ff15a1c7d31316
tim-vd-aardweg <tim-vd-aardweg@users.noreply.github.com>
2022-10-10 14:37:12 +0000

    autoformat: isort & black

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,17 +8,6 @@ from pydantic.error_wrappers import ValidationError
 
 from hydrolib.core.basemodel import DiskOnlyFileModel
 from hydrolib.core.io.dflowfm.bc.models import ForcingBase, ForcingModel
-from hydrolib.core.io.dimr.models import (
-    DIMR,
-    ComponentOrCouplerRef,
-    CoupledItem,
-    Coupler,
-    FMComponent,
-    Logger,
-    Parallel,
-    RRComponent,
-    StartGroup,
-)
 from hydrolib.core.io.dflowfm.ext.models import Boundary, ExtModel
 from hydrolib.core.io.dflowfm.friction.models import FrictGeneral
 from hydrolib.core.io.dflowfm.mdu.models import (
@@ -32,8 +21,19 @@ from hydrolib.core.io.dflowfm.mdu.models import (
     Restart,
     Sediment,
 )
-from hydrolib.core.io.rr.models import RainfallRunoffModel
 from hydrolib.core.io.dflowfm.xyz.models import XYZModel
+from hydrolib.core.io.dimr.models import (
+    DIMR,
+    ComponentOrCouplerRef,
+    CoupledItem,
+    Coupler,
+    FMComponent,
+    Logger,
+    Parallel,
+    RRComponent,
+    StartGroup,
+)
+from hydrolib.core.io.rr.models import RainfallRunoffModel
 
 from .io.rr.meteo.test_bui import BuiTestData
 from .utils import (
