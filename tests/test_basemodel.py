42a5ce7a4fdd743b7164f0994f5baf9dc9de9108
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2022-10-11 16:20:01 +0200

    #341: Fix test

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -297,11 +297,8 @@ class TestFileModel:
         file_path = test_input_dir / "resolve_casing_file_load_test" / "fm.mdu"
         model = FMModel(file_path, resolve_casing=True)
 
-        assert str(model.geometry.inifieldfile.filepath) == "initial/initialFields.ini"
-        assert (
-            str(model.geometry.inifieldfile.initial[0].datafile.filepath)
-            == "InitialWaterLevel.ini"
-        )
+        assert model.geometry.inifieldfile.filepath == Path("initial/initialFields.ini") 
+        assert model.geometry.inifieldfile.initial[0].datafile.filepath == Path("InitialWaterLevel.ini")
 
 
 class TestContextManagerFileLoadContext:
