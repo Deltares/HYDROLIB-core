2e6bc1968475b6daf386f09e9b45a88ffebfb8a6
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2022-10-13 13:00:46 +0200

    Fixed the default enum values for timeinterpolation and verticalinterpolation.

--- hydrolib/core/io/bc/models.py
+++ hydrolib/core/io/bc/models.py
@@ -361,7 +361,7 @@ class T3D(ForcingBase):
     """List[float]: The specification of the vertical positions."""
 
     vertinterpolation: VerticalInterpolation = Field(
-        VerticalInterpolation.linear, alias="vertInterpolation"
+        VerticalInterpolation.linear.value, alias="vertInterpolation"
     )
     """VerticalInterpolation: The type of vertical interpolation. Defaults to linear."""
 
@@ -369,7 +369,7 @@ class T3D(ForcingBase):
     """VerticalPositionType: The vertical position type of the verticalpositions values."""
 
     timeinterpolation: TimeInterpolation = Field(
-        TimeInterpolation.linear, alias="timeInterpolation"
+        TimeInterpolation.linear.value, alias="timeInterpolation"
     )
     """TimeInterpolation: The type of time interpolation. Defaults to linear."""
 
