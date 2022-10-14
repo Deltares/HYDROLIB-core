0f71dce8e32a5d11ef7ba4391c9ebfef90dfc047
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2022-10-14 17:25:45 +0200

    #288: Add comments for VolumeTables section MDU

--- hydrolib/core/io/mdu/models.py
+++ hydrolib/core/io/mdu/models.py
@@ -135,9 +135,9 @@ class VolumeTables(INIBasedModel):
     [UM Sec.A.1](https://content.oss.deltares.nl/delft3d/manuals/D-Flow_FM_User_Manual_1D2D.pdf#section.A.1).
     """
     class Comments(INIBasedModel.Comments):
-        usevolumetables: bool = Field("Use volume tables for 1D grid cells (see section 8.16) (1: yes, 0 = no).", alias="useVolumeTables")
-        increment: float = Field("The height increment for the volume tables [m].", alias="increment")
-        usevolumetablefile: bool = Field("Read and write the volume table from/to file (1: yes, 0= no).", alias="useVolumeTableFile")
+        usevolumetables: Optional[str] = Field("Use volume tables for 1D grid cells (see section 8.16) (1: yes, 0 = no).", alias="useVolumeTables")
+        increment: Optional[str] = Field("The height increment for the volume tables [m].", alias="increment")
+        usevolumetablefile: Optional[str] = Field("Read and write the volume table from/to file (1: yes, 0= no).", alias="useVolumeTableFile")
 
     comments: Comments = Comments()
 
