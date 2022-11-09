4c6723a7bfbd232c746fdb4b32483eeab235fc5b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2022-11-09 14:09:33 +0100

    #410: Extend imports in IO init.py

--- hydrolib/core/io/__init__.py
+++ hydrolib/core/io/__init__.py
@@ -1,3 +1,26 @@
-from hydrolib.core.io.dflowfm.mdu.models import *
+# DIMR model imports
 from hydrolib.core.io.dimr.models import *
+
+# dflowfm model imports
+from hydrolib.core.io.dflowfm.bc.models import *
+from hydrolib.core.io.dflowfm.common.models import *
+from hydrolib.core.io.dflowfm.crosssection.models import *
+from hydrolib.core.io.dflowfm.ext.models import *
+from hydrolib.core.io.dflowfm.friction.models import *
+from hydrolib.core.io.dflowfm.gui.models import *
+from hydrolib.core.io.dflowfm.ini.models import *
+from hydrolib.core.io.dflowfm.inifield.models import *
+from hydrolib.core.io.dflowfm.mdu.models import *
+from hydrolib.core.io.dflowfm.net.models import *
+from hydrolib.core.io.dflowfm.obs.models import *
+from hydrolib.core.io.dflowfm.obscrosssection.models import *
+from hydrolib.core.io.dflowfm.onedfield.models import *
+from hydrolib.core.io.dflowfm.polyfile.models import *
+from hydrolib.core.io.dflowfm.storagenode.models import *
+from hydrolib.core.io.dflowfm.structure.models import *
+from hydrolib.core.io.dflowfm.xyz.models import *
+
+# rr model imports
+from hydrolib.core.io.rr.meteo.models import *
 from hydrolib.core.io.rr.models import *
+from hydrolib.core.io.rr.topology.models import *
