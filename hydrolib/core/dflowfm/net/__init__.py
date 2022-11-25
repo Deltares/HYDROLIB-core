74076f4c62912dec53bb42e141fc9c6505e0504e
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2022-11-25 10:52:03 +0100

    Improved net imports.

---
+++ hydrolib/core/dflowfm/net/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    Mesh2d,
+    Branch,
+    Link1d2d,
+    Mesh1d,
+    Network,
+    NetworkModel,
+)
+
+__all__ = [
+    "Mesh2d",
+    "Branch",
+    "Link1d2d",
+    "Mesh1d",
+    "Network",
+    "NetworkModel",
+]
