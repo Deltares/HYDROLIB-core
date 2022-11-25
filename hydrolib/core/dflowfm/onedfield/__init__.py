427c3d995510849b6fc340aea0e7e97783a70f78
tim-vd-aardweg <tim-vd-aardweg@users.noreply.github.com>
2022-11-25 09:58:25 +0000

    autoformat: isort & black

--- hydrolib/core/dflowfm/onedfield/__init__.py
+++ hydrolib/core/dflowfm/onedfield/__init__.py
@@ -1,3 +1,3 @@
-from .models import OneDFieldGeneral, OneDFieldGlobal, OneDFieldBranch, OneDFieldModel
+from .models import OneDFieldBranch, OneDFieldGeneral, OneDFieldGlobal, OneDFieldModel
 
 __all__ = ["OneDFieldGeneral", "OneDFieldGlobal", "OneDFieldBranch", "OneDFieldModel"]
