e9d2dab273e309c164a1c6fe50b7b465f694300b
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2022-12-16 14:40:46 +0100

    Improved polyfile imports.

---
+++ hydrolib/core/dflowfm/polyfile/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    Description,
+    Metadata,
+    Point,
+    PolyObject,
+    PolyFile,
+)
+
+__all__ = [
+    "Description",
+    "Metadata",
+    "Point",
+    "PolyObject",
+    "PolyFile",
+]
