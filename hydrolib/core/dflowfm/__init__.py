f1dea8f267b261d0d634320e242a82469186b693
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2022-12-16 14:30:40 +0100

    Improved crosssection imports.

--- hydrolib/core/dflowfm/__init__.py
+++ hydrolib/core/dflowfm/__init__.py
@@ -1,2 +1,3 @@
 from .bc import *
 from .common import *
+from .crosssection import *
---
+++ hydrolib/core/dflowfm/crosssection/__init__.py
@@ -0,0 +1,29 @@
+from .models import (
+    CrossDefGeneral,
+    CrossLocGeneral,
+    CrossSectionDefinition,
+    CrossDefModel,
+    CircleCrsDef,
+    RectangleCrsDef,
+    ZWRiverCrsDef,
+    ZWCrsDef,
+    YZCrsDef,
+    XYZCrsDef,
+    CrossSection,
+    CrossLocModel,
+)
+
+__all__ = [
+    "CrossDefGeneral",
+    "CrossLocGeneral",
+    "CrossSectionDefinition",
+    "CrossDefModel",
+    "CircleCrsDef",
+    "RectangleCrsDef",
+    "ZWRiverCrsDef",
+    "ZWCrsDef",
+    "YZCrsDef",
+    "XYZCrsDef",
+    "CrossSection",
+    "CrossLocModel",
+]
