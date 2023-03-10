5688353f1d96e4795faae7847a333fb04a76a339
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-03-10 16:00:02 +0100

    #339: Cut first slash from relative windows path.

--- hydrolib/core/basemodel.py
+++ hydrolib/core/basemodel.py
@@ -317,7 +317,8 @@ class FilePathStyleResolver:
 
     @classmethod
     def _from_windows_to_posix_path(cls, windows_path: Path) -> Path:
-        windows_path = Path(str(windows_path).replace("\\", "/"))
+        windows_path_str = str(windows_path).replace("\\", "/").lstrip("/")
+        windows_path = Path(windows_path_str)
 
         is_relative = ":" not in windows_path.parts[0]
 
