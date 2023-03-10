29209159994a2310042edb864c4751568bab6a1b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-03-10 17:32:20 +0100

    #339: Extra test statements.

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -825,7 +825,18 @@ class TestFilePathStyleResolver:
     def test_convert_relative_windowslike_filepath_to_unixlike_filepath(
         self, windows_path: str
     ):
-        assert str(Path(windows_path)) == "test"
+        step1 = str(Path(windows_path))
+        assert step1 == '/path/to.file'
+        
+        step2 = step1.replace("\\", "/")
+        assert step2 == '/path/to.file'
+
+        step3 = step2.lstrip("/")
+        assert step3 == 'path/to.file'
+
+        step4 = Path(step3)
+        assert str(step4) == 'path/to.file'
+
         resolver = FilePathStyleResolver()
         unix_path = resolver.resolve(Path(windows_path), PathStyle.WINDOWSLIKE)
 
