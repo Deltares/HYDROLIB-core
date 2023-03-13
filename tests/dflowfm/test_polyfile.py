2a559cb4f602e7dea680df6c64153da91b374b7c
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2023-03-13 10:09:40 +0100

    #320 add comment tests using unsupported # for plifile

--- tests/dflowfm/test_polyfile.py
+++ tests/dflowfm/test_polyfile.py
@@ -671,6 +671,26 @@ name
                 ),
                 [((0, 4), "Expected a valid next point at line 3.")],
             ),
+            (
+                inspect.cleandoc(
+                    """
+                    *description
+                    name
+                    1  5   
+                    # 1.0 2.0 3.0 4.0 5.0 Comment after the values is invalid"""
+                ),
+                [((0, 4), "Expected a valid next point at line 3.")],
+            ),
+                        (
+                inspect.cleandoc(
+                    """
+                    *description
+                    name
+                    1  5   
+                    1.0 2.0 3.0 4.0 5.0 # Comment after the values is invalid"""
+                ),
+                [((0, 4), "Expected a valid next point at line 3.")],
+            ),
         ],
     )
     def test_invalid_block_correctly_raises_error(
