590810ea2b5dd11d7de657d100a2127ec2b39d19
Arthur van Dam <arthurvd@gmail.com>
2023-03-22 16:52:26 +0100

    Replace obscrs parser test by 3 MDU-based tests
    * with the new `assert_objects_equal(...)`

--- tests/dflowfm/test_mdu.py
+++ tests/dflowfm/test_mdu.py
@@ -15,10 +15,19 @@ from hydrolib.core.dflowfm.mdu.models import (
     ProcessFluxIntegration,
     VegetationModelNr,
 )
-from hydrolib.core.dflowfm.obscrosssection.models import ObservationCrossSectionModel
-from hydrolib.core.dflowfm.polyfile.models import PolyFile
+from hydrolib.core.dflowfm.obscrosssection.models import (
+    ObservationCrossSection,
+    ObservationCrossSectionModel,
+)
+from hydrolib.core.dflowfm.polyfile.models import (
+    Description,
+    Metadata,
+    Point,
+    PolyFile,
+    PolyObject,
+)
 
-from ..utils import WrapperTest, test_input_dir
+from ..utils import WrapperTest, assert_objects_equal, test_input_dir
 
 
 class TestModels:
@@ -98,6 +107,11 @@ class TestModels:
         assert isinstance(model.landboundaryfile[0], DiskOnlyFileModel)
 
 
+class PyTestMatchAny:
+    def __eq__(self, other):
+        return True
+
+
 class TestOutput:
     """Test class to test the [output] section in an MDU file."""
 
@@ -122,35 +136,129 @@ class TestOutput:
             len(fmmodel.output.crsfile[0].observationcrosssection[3].xcoordinates) == 2
         )
 
-    def test_mixed_obs_crs_files(self):
-        """Test the construction of correct types of observation crosssection
-        objects, when both old and new formatted input files are given."""
+    def _get_expected_polyobjects(self):
+        """Get PolyObject list that corresponds with example test file.
 
-        input = inspect.cleandoc(
-            f"""
-            [output]
-            CrsFile = {test_input_dir / 'dflowfm_individual_files/test.pli'} {test_input_dir / 'e02/f101_1D-boundaries/c01_steady-state-flow/ObservationPoints_crs.ini'}
-            """
-        )
-        parser = Parser(ParserConfig())
-        for l in input.splitlines():
-            parser.feed_line(l)
+        Use this for testing correct reading of PolyFile
+        `tests/data/input/obscrsfile_cases/test_crs.pli`.
 
-        document = parser.finalize()
-        wrapper = WrapperTest[Output].parse_obj({"val": document.sections[0]})
-        output = wrapper.val
+        Returns:
+            [PolyObject]: the objects contained in the fully loaded polyfile."""
 
-        assert len(output.crsfile) == 2
+        objects = [
+            PolyObject(
+                description=Description(content="\n description\n more description\n"),
+                metadata=Metadata(name="name1", n_rows=2, n_columns=2),
+                points=[Point(x=1, y=2, data=[]), Point(x=3, y=4, data=[])],
+            ),
+            PolyObject(
+                description=Description(
+                    content=r" oneline description (and last polyline will have NO comment)"
+                ),
+                metadata=Metadata(name="name2", n_rows=2, n_columns=2),
+                points=[Point(x=11, y=2, data=[]), Point(x=13, y=4, data=[])],
+            ),
+            PolyObject(
+                description=None,
+                metadata=Metadata(name="name3", n_rows=2, n_columns=2),
+                points=[Point(x=21, y=2, data=[]), Point(x=23, y=4, data=[])],
+            ),
+        ]
 
-        assert isinstance(output.crsfile[0], PolyFile)
-        assert isinstance(output.crsfile[1], ObservationCrossSectionModel)
+        return objects
+
+    def _get_expected_observationcrosssections(self):
+        """Get ObservationCrossSection list that corresponds with example test file.
+
+        Use this for testing correct reading of PolyFile
+        `tests/data/input/obscrsfile_cases/ObservationPoints_crs.ini`.
+
+        Returns:
+            [ObservationCrossSection]: the objects contained in the fully loaded obs_crs.ini file."""
+
+        objects = [
+            ObservationCrossSection(
+                name="ObservCross_Chg_T1",
+                branchid="T1",
+                chainage=1000,
+            ),
+            ObservationCrossSection(
+                name="ObservCross_xy_T2",
+                numcoordinates=2,
+                xcoordinates=[1000.0, 1000.0],
+                ycoordinates=[300.0, 200.0],
+            ),
+            ObservationCrossSection(
+                name="ObservCross_Chg_T3", branchid="T3", chainage=1000
+            ),
+            ObservationCrossSection(
+                name="ObservCross_xy_T4",
+                numcoordinates=2,
+                xcoordinates=[1000.0, 1000.0],
+                ycoordinates=[800.0, 700.0],
+            ),
+        ]
+
+        return objects
+
+    def _check_correct_polyobjects(self, loaded_polyfile: PolyFile):
+        expected_polyobjects = self._get_expected_polyobjects()
+
+        assert isinstance(loaded_polyfile, PolyFile)
 
         # Check recursive construction of PolyFile object
-        assert len(output.crsfile[0].objects) == 3
-        assert output.crsfile[0].objects[0].metadata.name == "name1"
-        assert len(output.crsfile[0].objects[2].points) == 2
+        assert loaded_polyfile.objects == expected_polyobjects
 
-        # Check recursive construction of ObservationCrossSectionModel object
-        assert len(output.crsfile[1].observationcrosssection) == 4
-        assert output.crsfile[1].observationcrosssection[0].name == "ObservCross_Chg_T1"
-        assert len(output.crsfile[1].observationcrosssection[3].xcoordinates) == 2
+    def _check_correct_obscrsobjects(
+        self, loaded_obscrsmodel: ObservationCrossSectionModel
+    ):
+        expected_obscrsobjects = self._get_expected_observationcrosssections()
+
+        assert isinstance(loaded_obscrsmodel, ObservationCrossSectionModel)
+
+        assert_objects_equal(
+            loaded_obscrsmodel.observationcrosssection,
+            expected_obscrsobjects,
+            ["comments"],
+        )
+
+    def test_loading_fmmodel_model_with_single_ini_crsfile(self):
+        file_path = (
+            test_input_dir
+            / "obscrsfile_cases"
+            / "single_ini"
+            / "emptymodel_with_obscrs.mdu"
+        )
+        model = FMModel(file_path)
+
+        assert len(model.output.crsfile) == 1
+
+        self._check_correct_obscrsobjects(model.output.crsfile[0])
+
+    def test_loading_fmmodel_model_with_single_pli_crsfiles(self):
+        file_path = (
+            test_input_dir
+            / "obscrsfile_cases"
+            / "single_pli"
+            / "emptymodel_with_obscrs.mdu"
+        )
+        model = FMModel(file_path)
+
+        assert len(model.output.crsfile) == 1
+
+        self._check_correct_polyobjects(model.output.crsfile[0])
+
+    def test_loading_fmmodel_model_with_both_ini_and_pli_crsfiles(self):
+        file_path = (
+            test_input_dir
+            / "obscrsfile_cases"
+            / "both_ini_and_pli"
+            / "emptymodel_with_obscrs.mdu"
+        )
+        model = FMModel(file_path)
+
+        assert len(model.output.crsfile) == 2
+
+        self._check_correct_polyobjects(model.output.crsfile[0])
+
+        self._check_correct_obscrsobjects(model.output.crsfile[1])
