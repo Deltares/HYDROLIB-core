72b5a6bcb141a829a1f7e7e36bdf03b621c245ff
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-03-24 14:56:52 +0100

    #369: Add initial implementation for Parser

--- hydrolib/core/dflowfm/extold/parser.py
+++ hydrolib/core/dflowfm/extold/parser.py
@@ -9,7 +9,7 @@ class Parser:
     def parse(filepath: Path) -> Dict:
         """Parses the file at the specified path to the forcing data.
 
-        If a line starts with an asterisk (*) is it considered a comment and will not be parsed.
+        If a line starts with an asterisk (*) it is considered a comment and will not be parsed.
 
         Args:
             path (Path): The path to parse the data from.
@@ -17,6 +17,24 @@ class Parser:
         Returns:
             Dict: A dictionary containing the forcing data under the key 'forcing'.
         """
-        data: Dict = dict(forcing=[])
+        
+        forcings = []
+        current_forcing = {}
 
-        return data
+        with filepath.open() as file:
+            for line in file:
+
+                line = line.strip()
+                if line.startswith("*"):
+                    continue
+
+                if len(line) == 0:
+                    if len(current_forcing) != 0:
+                        forcings.append(current_forcing)
+                    current_forcing = {}
+                    continue
+
+                key, value = line.split('=', 1)
+                current_forcing[key.strip()] = value.strip()
+
+        return dict(forcing=forcings)
