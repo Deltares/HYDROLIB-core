c03ab83324a6679989b066a1e5a2542592fcd420
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-03-29 10:06:51 +0200

    #369: Use ModelSaveSettings to write correct path.

--- hydrolib/core/dflowfm/extold/serializer.py
+++ hydrolib/core/dflowfm/extold/serializer.py
@@ -1,23 +1,17 @@
 from pathlib import Path
 from typing import Any, Dict
 
-from hydrolib.core.basemodel import (
-    DiskOnlyFileModel,
-    ModelSaveSettings,
-    SerializerConfig,
-)
+from hydrolib.core.basemodel import DiskOnlyFileModel, FileModel, ModelSaveSettings, SerializerConfig
 from hydrolib.core.dflowfm.extold.io import FORCING_FILE_ORDERED_FIELDS
+from hydrolib.core.utils import FilePathStyleConverter
 
 
 class Serializer:
     """Serializer class for serializing the forcing data of the `ExtOldModel` to file."""
 
-    def serialize(
-        path: Path,
-        data: Dict,
-        config: SerializerConfig,
-        save_settings: ModelSaveSettings,
-    ):
+    _file_path_style_converter = FilePathStyleConverter()
+
+    def serialize(path: Path, data: Dict, config: SerializerConfig, save_settings: ModelSaveSettings):
         """Serializes the provided data to file at the specified path.
 
         If a file already exists at the target location the file will be overwritten.
@@ -42,16 +36,20 @@ class Serializer:
                     if Serializer._skip_field_serialization(value):
                         continue
 
-                    value = Serializer._convert_value(value, config)
+                    value = Serializer._convert_value(value, config, save_settings)
 
                     f.write((f"{key}={value}\n"))
 
                 f.write(("\n"))
 
     @classmethod
-    def _convert_value(cls, value: Any, config: SerializerConfig) -> str:
+    def _convert_value(cls, value: Any, config: SerializerConfig, save_settings: ModelSaveSettings) -> str:
         if isinstance(value, float):
             return f"{value:{config.float_format}}"
+        if isinstance(value, FileModel):
+            return Serializer._file_path_style_converter.convert_from_os_style(
+                value.filepath, save_settings.path_style
+            )
 
         return str(value)
 
