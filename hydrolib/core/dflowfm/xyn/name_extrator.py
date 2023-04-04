315613298ce02ee225c8fb33edff51cbfe41d0d7
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2023-04-04 10:27:57 +0200

    Updated the name extractor

--- hydrolib/core/dflowfm/xyn/name_extrator.py
+++ hydrolib/core/dflowfm/xyn/name_extrator.py
@@ -11,12 +11,11 @@ class NameExtractor:
     @staticmethod
     def extract_name(text: str) -> str:
         """
-        Extract the first name from a given text string.
+        Extract the name from a given text string.
 
         This function is designed to handle names enclosed with single quotes
-        and normal unquoted names. For names enclosed with single quotes,
-        it will extract the name within the quotes. For normal names, it
-        will extract the first word in the text string.
+        and normal unquoted names. Names between single quotes may contain
+        spaces. Names themselves may not contain a single quote.
 
         Args:
         text (str): The input text string containing the name.
@@ -28,25 +27,38 @@ class NameExtractor:
         ValueError: If the input text is empty, contains only whitespace,
         or if the name contains a single quote not properly enclosed.
         """
+
+        def _extract_quoted_name(text: str) -> str:
+            if not text.endswith("'"):
+                raise ValueError(
+                    "Name that starts with a `'` should also end with a `'`"
+                )
+
+            name = text.strip("'")
+            if not name or name.isspace():
+                raise ValueError("Name cannot be empty.")
+
+            if "'" in name:
+                raise ValueError("Name cannot contain `'`.")
+
+            return name
+
+        def _extract_name(text: str) -> str:
+            if "'" in text:
+                raise ValueError("Name cannot contain `'`.")
+
+            name = text.split()
+            if len(name) == 1:
+                return name[0]
+
+            raise ValueError("Name cannot contain spaces.")
+
         if not text or text.isspace():
             raise ValueError("Name cannot be empty.")
 
         text = text.strip()
 
         if text.startswith("'"):
-            match = re.search(r"'(.*?)'", text)
-            if not match:
-                raise ValueError(
-                    "Name that starts with a `'` should also end with a `'`"
-                )
+            return _extract_quoted_name(text)
 
-            name = match.group(1).strip("'")
-            if len(name) == 0 or name.isspace():
-                raise ValueError("Name cannot be empty.")
-            else:
-                return name
-
-        if "'" in text:
-            raise ValueError("Name cannot contain `'`.")
-
-        return text.split()[0]
+        return _extract_name(text)
