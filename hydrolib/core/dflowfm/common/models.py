8cef82c25b72e5789fa10764b771421cabe0ce18
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2023-04-11 11:22:16 +0200

    #369: Add common Operand class for inifield and extold

--- hydrolib/core/dflowfm/common/models.py
+++ hydrolib/core/dflowfm/common/models.py
@@ -15,3 +15,22 @@ class LocationType(str, Enum):
 
     all = "all"
     """str: Denotes that both 1D and 2D locations may be selected."""
+
+class Operand(str, Enum):
+    """
+    Enum class containing the valid values for the operand
+    attribute in several subclasses of AbstractIniField and ExtOldForcing.
+    """
+
+    override = "O"
+    """Existing values are overwritten with the provided values."""
+    append = "A"
+    """Provided values are used where existing values are missing."""
+    add = "+"
+    """Existing values are summed with the provided values."""
+    mult = "*"
+    """Existing values are multiplied with the provided values."""
+    max = "X"
+    """The maximum values of the existing values and provided values are used."""
+    min = "N"
+    """The minimum values of the existing values and provided values are used."""
