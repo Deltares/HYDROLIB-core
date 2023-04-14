e73968956ee12d8028641b2ea932b72444dd596a
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2023-04-14 14:48:04 +0200

    Added UTF8 encoding

--- hydrolib/core/dflowfm/tim/parser.py
+++ hydrolib/core/dflowfm/tim/parser.py
@@ -33,7 +33,7 @@ class TimParser:
         comments: List[str] = []
         timeseries: List[TimData] = []
 
-        with filepath.open() as file:
+        with filepath.open(encoding="utf8") as file:
             lines = file.readlines()
             comments, start_timeseries_index = TimParser._read_header_comments(lines)
             timeseries = TimParser._read_time_series_data(lines, start_timeseries_index)
