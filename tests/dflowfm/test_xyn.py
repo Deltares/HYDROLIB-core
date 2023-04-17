039ad371066ce3a45d7cfcc1cdedd450d8632205
Tim van den Aardweg <71257004+tim-vd-aardweg@users.noreply.github.com>
2023-04-17 14:18:01 +0200

    Add support for station names surrounded by double quotes
    Refs: #537

--- tests/dflowfm/test_xyn.py
+++ tests/dflowfm/test_xyn.py
@@ -19,7 +19,7 @@ class TestXYNParser:
         file_content = """
             *This is a comment and should not be parsed.
             1.1 2.2 'ObservationPoint_2D_01'
-            3.3 4.4 'ObservationPoint_2D_02'
+            3.3 4.4 "ObservationPoint_2D_02"
         """
 
         expected_result = {
@@ -31,22 +31,42 @@ class TestXYNParser:
 
         with create_temp_file(file_content, "test.xyn") as xyn_file:
             parsed_contents = XYNParser.parse(xyn_file)
-            assert expected_result == parsed_contents
+        assert expected_result == parsed_contents
 
-    def test_parse_xyn_file_with_quoted_name_removes_quotes(self):
+    def test_parse_xyn_file_with_single_quoted_name_removes_quotes_and_keeps_whitespace(
+        self,
+    ):
         file_content = """
-            1.1 2.2 'ObservationPoint_2D_01'
+            1.1 2.2 'ObservationPoint 2D 01'
         """
 
         expected_result = {
             "points": [
-                {"x": "1.1", "y": "2.2", "n": "ObservationPoint_2D_01"},
+                {"x": "1.1", "y": "2.2", "n": "ObservationPoint 2D 01"},
             ]
         }
 
         with create_temp_file(file_content, "test.xyn") as xyn_file:
             parsed_contents = XYNParser.parse(xyn_file)
-            assert expected_result == parsed_contents
+        assert expected_result == parsed_contents
+
+    def test_parse_xyn_file_with_double_quoted_name_removes_quotes_and_keeps_whitespace(
+        self,
+    ):
+        file_content = """
+            1.1 2.2 "ObservationPoint 2D 01"
+        """
+
+        expected_result = {
+            "points": [
+                {"x": "1.1", "y": "2.2", "n": "ObservationPoint 2D 01"},
+            ]
+        }
+
+        with create_temp_file(file_content, "test.xyn") as xyn_file:
+            parsed_contents = XYNParser.parse(xyn_file)
+
+        assert expected_result == parsed_contents
 
     def test_parse_xyn_file_with_too_many_columns_raises_error(self):
         name = "'ObservationPoint_2D_01' This is too much content"
@@ -54,7 +74,7 @@ class TestXYNParser:
 
         with pytest.raises(ValueError) as error:
             with create_temp_file(file_content, "test.xyn") as xyn_file:
-                expected_message = f"Error parsing XYN file '{xyn_file}', line 1. Name `{name}` contains whitespace, so should be enclosed in single quotes."
+                expected_message = f"Error parsing XYN file '{xyn_file}', line 1. Name `{name}` contains whitespace, so should be enclosed in quotes."
                 _ = XYNParser.parse(xyn_file)
 
         assert expected_message in str(error.value)
@@ -79,7 +99,7 @@ class TestXYNSerializer:
 
             with open(xyn_file) as file:
                 file_content = file.readlines()
-                assert file_content == expected_file_content
+        assert file_content == expected_file_content
 
 
 class TestXYNModel:
@@ -131,7 +151,8 @@ class TestXYNPoint:
     @pytest.mark.parametrize(
         ("name"),
         [
-            pytest.param("'nameWithQuote", id="Name with quote"),
+            pytest.param("nameWith'SingleQuote", id="Name with single quote"),
+            pytest.param('nameWith"DoubleQuote', id="Name with double quote"),
             pytest.param(None, id="None value"),
             pytest.param("", id="Empty string"),
             pytest.param("     ", id="Whitespace only"),
