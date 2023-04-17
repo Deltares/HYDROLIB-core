8f6b1f2cb36de99d8a532fcab94feda5af65a7c9
Marlon Vermeulen <102953403+MRVermeulenDeltares@users.noreply.github.com>
2023-04-17 09:07:15 +0200

    chore: Improved TimModel implementation
    refs: #511

--- hydrolib/core/dflowfm/tim/serializer.py
+++ hydrolib/core/dflowfm/tim/serializer.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 from hydrolib.core.basemodel import ModelSaveSettings, SerializerConfig
 
@@ -18,18 +18,21 @@ class TimSerializer:
     @staticmethod
     def serialize(
         path: Path,
-        data: Dict[str, Any],
+        data: Dict[str, List[Any]],
         config: TimSerializerConfig,
         save_settings: ModelSaveSettings,
     ) -> None:
         """
-        Serializes timeseries data to a file in .tim format.
+        Serialize timeseries data to a file in .tim format.
 
         Args:
             path (Path): The path to the destination .tim file.
-            data (Dict[str, Any]): The timeseries data to be serialized. The data should be provided as a dictionary with the following keys:
-                - 'comments' (List[str]): A list of comments to be included at the beginning of the file. Each comment should be a string.
-                - 'timeseries' (Dict[float, List[float]]): A dictionary representing the timeseries data, where the key is the time and the value the values at that time.
+            data (Dict[str, List[Any]]): A dictionary with keys "comments" and "timeseries".
+            - "comments" represents comments found at the start of the file.
+            - "timeseries" is a list of dictionaries with the key as "time" and values as "data".
+                - "time" is a time as a string.
+                - "data" is data as a list of strings.
+
             config (TimSerializerConfig): The serialization configuration settings.
             save_settings (ModelSaveSettings): The save settings to be used.
         """
@@ -45,14 +48,16 @@ class TimSerializer:
             file.write(file_content)
 
     @staticmethod
-    def _serialize_comment_lines(data) -> List[str]:
+    def _serialize_comment_lines(data: Dict[str, List[Any]]) -> List[str]:
         commentlines = []
         for comment in data["comments"]:
             commentlines.append(f"#{comment}")
         return commentlines
 
     @staticmethod
-    def _serialize_timeseries_lines(data, config) -> List[str]:
+    def _serialize_timeseries_lines(
+        data: Dict[str, List[Any]], config: TimSerializerConfig
+    ) -> List[str]:
         format_float = lambda v: f"{v:{config.float_format}}"
         timeseriesblock = TimSerializer._serialize_to_timeseries_block(
             data, format_float
@@ -63,9 +68,13 @@ class TimSerializer:
         return timeserieslines
 
     @staticmethod
-    def _serialize_to_timeseries_block(data, format_float) -> TimeSeriesBlock:
+    def _serialize_to_timeseries_block(
+        data: Dict[str, List[Any]], format_float: Callable[[float], str]
+    ) -> TimeSeriesBlock:
         timeseries_block: TimeSeriesBlock = []
-        for time, row_elements in data["timeseries"].items():
+        for timeseries in data["timeseries"]:
+            time = timeseries["time"]
+            row_elements = timeseries["data"]
             timeseries_row = [format_float(time)] + [
                 format_float(value) for value in row_elements
             ]
@@ -73,7 +82,9 @@ class TimSerializer:
         return timeseries_block
 
     @staticmethod
-    def _serialize_timeseries_to_lines(timeseries_block, config) -> List[str]:
+    def _serialize_timeseries_to_lines(
+        timeseries_block: TimeSeriesBlock, config: TimSerializerConfig
+    ) -> List[str]:
         # Make sure the columns are aligned and have the proper spacing
         column_space = " " * config.column_spacing
         column_lengths = TimSerializer._get_column_lengths(timeseries_block)
@@ -97,7 +108,7 @@ class TimSerializer:
         return " " * max(max_length - value_length, 0)
 
     @staticmethod
-    def _serialize_file_content(timeserieslines, commentlines):
+    def _serialize_file_content(timeserieslines: List[str], commentlines: List[str]):
         lines = []
         lines.extend(commentlines)
         lines.extend(timeserieslines)
