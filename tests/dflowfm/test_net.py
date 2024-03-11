3e72e79437431ac90563c8fd22cbb45bab378098
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-03-11 14:40:32 +0100

    chore: Add testcase for multiple grids in mesh2d (#607)

--- tests/dflowfm/test_net.py
+++ tests/dflowfm/test_net.py
@@ -180,8 +180,29 @@ def test_create_2d():
     mesh2d.create_rectilinear(extent=bbox, dx=0.5, dy=0.75)
 
     mesh2d_output = mesh2d.get_mesh2d()
+    assert np.isclose(min(mesh2d_output.node_x), 1)
+    assert np.isclose(max(mesh2d_output.node_x), 3)
+    assert np.isclose(min(mesh2d_output.node_y), -2)
+    assert np.isclose(max(mesh2d_output.node_y), 4)
     assert mesh2d_output.node_x.size == 45
     assert mesh2d_output.edge_nodes.size == 152
+
+
+def test_create_2d_multiple_grids():
+    bbox = (1.0, -2.0, 3.0, 4.0)
+    bbox2 = (-2.0, -2.0, 0.0, 4.0)
+
+    mesh2d = Mesh2d(meshkernel=MeshKernel())
+    mesh2d.create_rectilinear(extent=bbox, dx=0.5, dy=0.75)
+    mesh2d.create_rectilinear(extent=bbox2, dx=0.5, dy=0.75)
+
+    mesh2d_output = mesh2d.get_mesh2d()
+    assert np.isclose(min(mesh2d_output.node_x), -2)
+    assert np.isclose(max(mesh2d_output.node_x), 3)
+    assert np.isclose(min(mesh2d_output.node_y), -2)
+    assert np.isclose(max(mesh2d_output.node_y), 4)
+    assert mesh2d_output.node_x.size == 90
+    assert mesh2d_output.edge_nodes.size == 304
 
 
 @pytest.mark.parametrize(
