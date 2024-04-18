3a521645d841fdd368e64343cf9fbbaf8df67a80
tim-vd-aardweg <tim-vd-aardweg@users.noreply.github.com>
2024-04-18 12:53:52 +0000

    autoformat: isort & black

--- hydrolib/core/dflowfm/ini/models.py
+++ hydrolib/core/dflowfm/ini/models.py
@@ -1,8 +1,8 @@
 import logging
-from re import compile
 from abc import ABC
 from enum import Enum
 from math import isnan
+from re import compile
 from typing import Any, Callable, List, Literal, Optional, Set, Type, Union
 
 from pydantic.v1 import Extra, Field, root_validator
@@ -47,7 +47,9 @@ class INIBasedModel(BaseModel, ABC):
 
     _header: str
     _file_path_style_converter = FilePathStyleConverter()
-    _scientific_notation_regex = compile(r"([\d.]+)([dD])([+-]?\d{1,3})")  # matches a float: 1d9, 1D-3, 1.D+4, etc.
+    _scientific_notation_regex = compile(
+        r"([\d.]+)([dD])([+-]?\d{1,3})"
+    )  # matches a float: 1d9, 1D-3, 1.D+4, etc.
 
     class Config:
         extra = Extra.ignore
--- tests/dflowfm/ini/test_models.py
+++ tests/dflowfm/ini/test_models.py
@@ -5,6 +5,7 @@ import pytest
 from pydantic.v1.error_wrappers import ValidationError
 
 from hydrolib.core.dflowfm.ini.models import DataBlockINIBasedModel, INIBasedModel
+
 from ...utils import error_occurs_only_once
 
 
@@ -52,62 +53,97 @@ class TestINIBasedModel:
     _random_list_of_floats: List[float] = [12.34, 56.78]
 
     @pytest.mark.parametrize("string_value", ["1d0", "1d-2", "1d+2", "1.d+2", "-1.d-2"])
-    def test_scientific_notation_for_string_field_is_parsed_as_string(self, string_value: str):
+    def test_scientific_notation_for_string_field_is_parsed_as_string(
+        self, string_value: str
+    ):
 
-        test_model = self.INIBasedModelTest(id=string_value, float_value=self._random_float,
-                                            float_values=self._random_list_of_floats)
+        test_model = self.INIBasedModelTest(
+            id=string_value,
+            float_value=self._random_float,
+            float_values=self._random_list_of_floats,
+        )
 
         assert test_model.id == string_value
         assert test_model.float_value == pytest.approx(self._random_float)
         assert test_model.float_values == pytest.approx(self._random_list_of_floats)
 
-    @pytest.mark.parametrize("float_as_string, expected_value", [
-        ("1d0", 1e0),
-        ("1d-2", 1e-2),
-        ("1d+2", 1e+2),
-        ("1.d+2", 1.e+2),
-        ("-1.d-2", -1.e-2)]
-                             )
-    def test_scientific_notation_for_float_field_is_parsed_as_float(self, float_as_string: str, expected_value: float):
-        test_model = self.INIBasedModelTest(id=self._random_string, float_value=float_as_string,
-                                            float_values=self._random_list_of_floats)
+    @pytest.mark.parametrize(
+        "float_as_string, expected_value",
+        [
+            ("1d0", 1e0),
+            ("1d-2", 1e-2),
+            ("1d+2", 1e2),
+            ("1.d+2", 1.0e2),
+            ("-1.d-2", -1.0e-2),
+        ],
+    )
+    def test_scientific_notation_for_float_field_is_parsed_as_float(
+        self, float_as_string: str, expected_value: float
+    ):
+        test_model = self.INIBasedModelTest(
+            id=self._random_string,
+            float_value=float_as_string,
+            float_values=self._random_list_of_floats,
+        )
 
         assert test_model.id == self._random_string
         assert test_model.float_value == pytest.approx(expected_value)
         assert test_model.float_values == pytest.approx(self._random_list_of_floats)
 
-    @pytest.mark.parametrize("floats_as_strings, expected_values", [
-        (["1d0", "1d-2"], [1e0, 1e-2]),
-        (["1d+2", "1.d+2", "-1.d-2"], [1e+2, 1.e+2, -1.e-2]),
-    ])
-    def test_scientific_notation_for_list_of_float_field_is_parsed_as_list_of_floats(self, floats_as_strings: List[str],
-                                                                                     expected_values: List[float]):
-        test_model = self.INIBasedModelTest(id=self._random_string, float_value=self._random_float,
-                                            float_values=floats_as_strings)
+    @pytest.mark.parametrize(
+        "floats_as_strings, expected_values",
+        [
+            (["1d0", "1d-2"], [1e0, 1e-2]),
+            (["1d+2", "1.d+2", "-1.d-2"], [1e2, 1.0e2, -1.0e-2]),
+        ],
+    )
+    def test_scientific_notation_for_list_of_float_field_is_parsed_as_list_of_floats(
+        self, floats_as_strings: List[str], expected_values: List[float]
+    ):
+        test_model = self.INIBasedModelTest(
+            id=self._random_string,
+            float_value=self._random_float,
+            float_values=floats_as_strings,
+        )
 
         assert test_model.id == self._random_string
         assert test_model.float_value == pytest.approx(self._random_float)
         assert test_model.float_values == pytest.approx(expected_values)
 
-    def test_setting_string_attribute_with_scientific_notation_correctly_parses_value(self):
-        test_model = self.INIBasedModelTest(id=self._random_string, float_value=self._random_float,
-                                            float_values=self._random_list_of_floats)
+    def test_setting_string_attribute_with_scientific_notation_correctly_parses_value(
+        self,
+    ):
+        test_model = self.INIBasedModelTest(
+            id=self._random_string,
+            float_value=self._random_float,
+            float_values=self._random_list_of_floats,
+        )
 
         test_model.id = "1d1"
 
         assert test_model.id == "1d1"
 
-    def test_setting_float_attribute_with_scientific_notation_correctly_parses_value(self):
-        test_model = self.INIBasedModelTest(id=self._random_string, float_value=self._random_float,
-                                            float_values=self._random_list_of_floats)
+    def test_setting_float_attribute_with_scientific_notation_correctly_parses_value(
+        self,
+    ):
+        test_model = self.INIBasedModelTest(
+            id=self._random_string,
+            float_value=self._random_float,
+            float_values=self._random_list_of_floats,
+        )
 
         test_model.float_value = "1d1"
 
         assert test_model.float_value == pytest.approx(1e1)
 
-    def test_setting_list_of_floats_attribute_with_scientific_notation_correctly_parses_values(self):
-        test_model = self.INIBasedModelTest(id=self._random_string, float_value=self._random_float,
-                                            float_values=self._random_list_of_floats)
+    def test_setting_list_of_floats_attribute_with_scientific_notation_correctly_parses_values(
+        self,
+    ):
+        test_model = self.INIBasedModelTest(
+            id=self._random_string,
+            float_value=self._random_float,
+            float_values=self._random_list_of_floats,
+        )
 
         test_model.float_values = ["1d1", "2d-1"]
 
