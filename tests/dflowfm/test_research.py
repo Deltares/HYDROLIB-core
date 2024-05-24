6db2937db0333e0754033d667b97ce8c2468f0c4
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2024-05-24 10:48:31 +0200

    #634: Fixed float comparisons.

--- tests/dflowfm/test_research.py
+++ tests/dflowfm/test_research.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hydrolib.core.dflowfm.research.models import (
     ResearchFMModel,
     ResearchGeneral,
@@ -41,11 +43,13 @@ class TestResearchFMModel:
         # there are too many research keywords to test, so here we just assert a couple of them
         assert model.general.research_inputspecific == False
         assert model.geometry.research_helmert == False
-        assert model.numerics.research_epseps == 1e-32
-        assert model.physics.research_uniffrictcoef1d2d == 2.3e-2
+        assert model.numerics.research_epseps == pytest.approx(1e-32)
+        assert model.physics.research_uniffrictcoef1d2d == pytest.approx(2.3e-2)
         assert model.sediment.research_implicitfallvelocity == 1
         assert model.wind.research_wind_eachstep == 0
         assert model.waves.research_threedwaveboundarylayer == 1
-        assert model.time.research_dtfacmax == 1.1
-        assert model.trachytopes.research_trtmnh == 0.1
-        assert model.output.research_mbainterval == 0.0
+        assert model.time.research_dtfacmax == pytest.approx(1.1)
+        assert model.trachytopes.research_trtmnh == pytest.approx(0.1)
+        assert model.output.research_mbainterval == pytest.approx(0.0)
+
+
