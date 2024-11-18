f99841e3d039d1952a27c12b9a8c171b9eafb038
Mostafa Farrag <moah.farag@gmail.com>
2024-11-18 18:38:01 +0100

    test the `InitialConditionConverter` class

---
+++ tests/tools/test_converters.py
@@ -0,0 +1,16 @@
+from hydrolib.core.dflowfm.extold.models import ExtOldForcing, ExtOldQuantity
+from hydrolib.tools.ext_old_to_new.initial_condition_converter import InitialConditionConverter
+
+
+def test_initial_condition_converter():
+    forcing = ExtOldForcing(
+        quantity= ExtOldQuantity.InitialWaterLevel,
+        filename="iniwaterlevel.pol",
+        filetype=10,  # "Polyline"
+        method="4",  # "Interpolate space",
+        operand="O",
+    )
+
+    new_quantity_block = InitialConditionConverter().convert(forcing)
+    new_quantity_block.datafiletype = "polygon"
+    new_quantity_block.interpolationmethod = "constant"
