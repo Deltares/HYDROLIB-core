9d516a842dd91e24f0586053c445681f2041e938
Mostafa Farrag <moah.farag@gmail.com>
2024-11-18 14:37:06 +0100

    import the used function directly at the top of the file do not use "module.function"

--- tests/tools/test_ext_old_to_new.py
+++ tests/tools/test_ext_old_to_new.py
@@ -1,19 +1,24 @@
 from pathlib import Path
 from hydrolib.tools.ext_old_to_new import main_converter
+from hydrolib.tools.ext_old_to_new.main_converter import \
+    ext_old_to_new_from_mdu, \
+    ext_old_to_new_dir_recursive, \
+    ext_old_to_new
+
 
 
 class TestExtOldToNew:
     def test_wind_combi_uniform_curvi(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
         mdu_filename = input_files_dir.joinpath("e02/f011_wind/c081_combi_uniform_curvi/windcase.mdu")
-        main_converter.ext_old_to_new_from_mdu(mdu_filename)
+        ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
 
     def test_extrapolate_slr(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
         mdu_filename = input_files_dir.joinpath("e02/f006_external_forcing/c011_extrapolate_slr/slrextrapol.mdu")
-        main_converter.ext_old_to_new_from_mdu(mdu_filename)
+        ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
 
@@ -22,12 +27,13 @@ class TestExtOldToNew:
         mdu_filename = input_files_dir.joinpath(
             "e02/f006_external_forcing/c020_basinnofriction_squares/basinsquares.mdu"
         )
-        main_converter.ext_old_to_new_from_mdu(mdu_filename)
+        ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
 
-    def test_recursive(self, capsys):
+    def test_recursive(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        main_converter.ext_old_to_new_dir_recursive(f"{test_input_dir}/e02/f006_external_forcing")
+        path = input_files_dir.joinpath("e02/f006_external_forcing")
+        ext_old_to_new_dir_recursive(path)
 
 
