fbad133ab32d16c73806f0d6995a82fd3c07164a
Mostafa Farrag <moah.farag@gmail.com>
2024-11-18 14:29:49 +0100

    reformat the test_ext_old_to_new.py file to use fixtures instead of the tests.utils module

--- tests/tools/test_ext_old_to_new.py
+++ tests/tools/test_ext_old_to_new.py
@@ -1,48 +1,27 @@
+from pathlib import Path
 from hydrolib.tools.ext_old_to_new import main_converter
-
-from tests.utils import (
-    test_input_dir,
-)
 
 
 class TestExtOldToNew:
-    def test_wind_combi_uniform_curvi(self, capsys):
+    def test_wind_combi_uniform_curvi(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = (
-            test_input_dir
-            / "e02"
-            / "f011_wind"
-            / "c081_combi_uniform_curvi"
-            / "windcase.mdu"
-        )
-
+        mdu_filename = input_files_dir.joinpath("e02/f011_wind/c081_combi_uniform_curvi/windcase.mdu")
         main_converter.ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
 
-    def test_extrapolate_slr(self, capsys):
+    def test_extrapolate_slr(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = (
-            test_input_dir
-            / "e02"
-            / "f006_external_forcing"
-            / "c011_extrapolate_slr"
-            / "slrextrapol.mdu"
-        )
+        mdu_filename = input_files_dir.joinpath("e02/f006_external_forcing/c011_extrapolate_slr/slrextrapol.mdu")
         main_converter.ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
 
-    def test_basinsquares(self, capsys):
+    def test_basinsquares(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = (
-            test_input_dir
-            / "e02"
-            / "f006_external_forcing"
-            / "c020_basinnofriction_squares"
-            / "basinsquares.mdu"
+        mdu_filename = input_files_dir.joinpath(
+            "e02/f006_external_forcing/c020_basinnofriction_squares/basinsquares.mdu"
         )
-
         main_converter.ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(f"Could not read {mdu_filename} as a valid FM model:")
