ed30f2c1a3b0af82f822b5d71fedfa7627245cc2
Mostafa Farrag <moah.farag@gmail.com>
2024-11-18 14:40:31 +0100

    reformat test file

--- tests/tools/test_main_converter.py
+++ tests/tools/test_main_converter.py
@@ -6,7 +6,6 @@ from hydrolib.tools.ext_old_to_new.main_converter import \
     ext_old_to_new
 
 
-
 class TestExtOldToNew:
     def test_wind_combi_uniform_curvi(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
@@ -35,11 +34,3 @@ class TestExtOldToNew:
         main_converter._verbose = True
         path = input_files_dir.joinpath("e02/f006_external_forcing")
         ext_old_to_new_dir_recursive(path)
-
-    def test_trial(self):
-        path = "tests/data/input/old-external-forcing.ext"
-        new_ext_file = "tests/data/new-external-forcing.ext"
-        new_initial_file = "tests/data/new-initial-conditions.ext"
-        new_structure_file = "tests/data/new-structure.ext"
-        result = ext_old_to_new(path, new_ext_file, new_initial_file, new_structure_file)
-        print(result)
