6c80dbc520e823af15770035adfcf2d73e1d9ec3
Mostafa Farrag <moah.farag@gmail.com>
2024-11-18 15:48:14 +0100

    test the `_read_ext_old_data` function

--- tests/tools/test_main_converter.py
+++ tests/tools/test_main_converter.py
@@ -1,8 +1,10 @@
+from typing import List
 from pathlib import Path
 from hydrolib.tools.ext_old_to_new import main_converter
 from hydrolib.tools.ext_old_to_new.main_converter import \
     ext_old_to_new_from_mdu, \
     ext_old_to_new_dir_recursive, \
+    _read_ext_old_data,\
     ext_old_to_new
 
 
@@ -34,3 +36,18 @@ class TestExtOldToNew:
         main_converter._verbose = True
         path = input_files_dir.joinpath("e02/f006_external_forcing")
         ext_old_to_new_dir_recursive(path)
+
+def test__read_ext_old_data(
+        capsys, old_forcing_file: Path, old_forcing_file_quantities: List[str], old_forcing_comment_len: int
+):
+    model = _read_ext_old_data(old_forcing_file)
+    assert len(model.forcing) == len(old_forcing_file_quantities)
+    assert len(model.comment) == old_forcing_comment_len
+    quantities = [forcing.quantity for forcing in model.forcing]
+    assert all([quantity in old_forcing_file_quantities for quantity in quantities])
+    # test verbose
+    main_converter._verbose = True
+    _read_ext_old_data(old_forcing_file)
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert captured.out.startswith(f"Read {(len(old_forcing_file_quantities))} forcing blocks from {old_forcing_file}.")
