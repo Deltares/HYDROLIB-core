532245686b69dd94a2598584ec70daf9e49e6ede
Mostafa Farrag <moah.farag@gmail.com>
2024-12-03 16:50:12 +0100

    add tests for different polyline cases (basic case, and with label)

--- tests/dflowfm/polyfile/test_polyline_models.py
+++ tests/dflowfm/polyfile/test_polyline_models.py
@@ -2,22 +2,24 @@ from pathlib import Path
 
 import pytest
 
-from hydrolib.core.dflowfm.polyfile.models import PolyFile
+from hydrolib.core.dflowfm.polyfile.models import Point, PolyFile
 
 
 def test_with_label(polylines_dir: Path):
     """
     The test check a simple 2*2 polyline file with a label
     Now the polyfile.parser does not support having any labels in the file.
-    So the test should raise a ValueError.
+    parser will ignore the label and read the file as a normal polyline file.
     tfl_01
         2      2
-        0.00000000000000000	0.00000000000000000e #zee
-        0.00000000000000000	2.00000000000000000e #zee
+        0.00000000000000000	0.00000000000000000 #zee
+        0.00000000000000000	2.00000000000000000 #zee
     """
     path = polylines_dir.joinpath("polyline-no-z-with-label.pli")
-    with pytest.raises(ValueError):
-        PolyFile(path)
+    polyline = PolyFile(path)
+    assert polyline.has_z_values is False
+    assert polyline.filepath == path
+    assert polyline.save_location == path.absolute()
 
 
 def test_without_z(polylines_dir: Path):
@@ -33,3 +35,40 @@ def test_without_z(polylines_dir: Path):
     assert polyline.has_z_values is False
     assert polyline.filepath == path
     assert polyline.save_location == path.absolute()
+    points = polyline.objects[0].points
+    assert points[0] == Point(x=-80, y=-50, z=None, data=[])
+    assert points[1] == Point(x=-80, y=550, z=None, data=[])
+
+
+def test_with_z_and_pli_extension(polylines_dir: Path):
+    """
+    The test check a 2*2 polyline file with z values but the extension is pli not pliz.
+    the parser will ignore the z values and read the file as a normal polyline file.
+    tfl_01
+        2      2
+        0.00000000000000000	0.00000000000000000 5
+        0.00000000000000000	2.00000000000000000 5
+    """
+    path = polylines_dir.joinpath("polyline-with-z-no-label.pli")
+    polyline = PolyFile(path)
+    assert polyline.has_z_values is False
+    assert polyline.filepath == path
+    assert polyline.save_location == path.absolute()
+    points = polyline.objects[0].points
+    assert points[0] == Point(x=0, y=0, z=None, data=[])
+    assert points[1] == Point(x=0, y=2, z=None, data=[])
+
+
+def test_with_z_and_pliz_extension(polylines_dir: Path):
+    """
+    The test check a 2*2 polyline file with z values, the extension is correct but the dimensions are 2*2.
+    not 2*3
+    the parser only reads the length of the dimensions in the second line and ignores the z values.
+    tfl_01
+        2      2
+        0.00000000000000000	0.00000000000000000 5
+        0.00000000000000000	2.00000000000000000 5
+    """
+    path = polylines_dir.joinpath("polyline-with-z-no-label.pliz")
+    with pytest.raises(ValueError):
+        PolyFile(path)
