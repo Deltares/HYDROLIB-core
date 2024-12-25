e29c1d912f68b0da2fb31f3e56874350f17454f7
Mostafa Farrag <moah.farag@gmail.com>
2024-12-25 18:29:09 +0100

    rename test

--- tests/tools/test_main_converter.py
+++ tests/tools/test_main_converter.py
@@ -16,12 +16,13 @@ from hydrolib.tools.ext_old_to_new.main_converter import (
 )
 
 
-class TestExtOldToNew:
+class TestExtOldToNewFromMDU:
     def test_wind_combi_uniform_curvi(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = input_files_dir.joinpath(
-            "e02/f011_wind/c081_combi_uniform_curvi/windcase.mdu"
+        mdu_filename = (
+            input_files_dir / "e02/f011_wind/c081_combi_uniform_curvi/windcase.mdu"
         )
+
         ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(
@@ -30,9 +31,11 @@ class TestExtOldToNew:
 
     def test_extrapolate_slr(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = input_files_dir.joinpath(
-            "e02/f006_external_forcing/c011_extrapolate_slr/slrextrapol.mdu"
+        mdu_filename = (
+            input_files_dir
+            / "e02/f006_external_forcing/c011_extrapolate_slr/slrextrapol.mdu"
         )
+
         ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(
@@ -41,9 +44,11 @@ class TestExtOldToNew:
 
     def test_basinsquares(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        mdu_filename = input_files_dir.joinpath(
-            "e02/f006_external_forcing/c020_basinnofriction_squares/basinsquares.mdu"
+        mdu_filename = (
+            input_files_dir
+            / "e02/f006_external_forcing/c020_basinnofriction_squares/basinsquares.mdu"
         )
+
         ext_old_to_new_from_mdu(mdu_filename)
         captured = capsys.readouterr()
         assert captured.out.startswith(
@@ -52,7 +57,7 @@ class TestExtOldToNew:
 
     def test_recursive(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
-        path = input_files_dir.joinpath("e02/f006_external_forcing")
+        path = input_files_dir / "e02/f006_external_forcing"
         ext_old_to_new_dir_recursive(path)
 
 
