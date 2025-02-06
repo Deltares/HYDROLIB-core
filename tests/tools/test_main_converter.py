23ee07d4f21911b07207a3eb5ea31dc72ecd46af
Mostafa Farrag <moah.farag@gmail.com>
2025-02-06 16:33:57 +0100

    patch the save method in order not to save test files

--- tests/tools/test_main_converter.py
+++ tests/tools/test_main_converter.py
@@ -48,7 +48,8 @@ class TestExtOldToNewFromMDU:
     def test_recursive(self, capsys, input_files_dir: Path):
         main_converter._verbose = True
         path = input_files_dir / "e02/f006_external_forcing"
-        ext_old_to_new_dir_recursive(path, suppress_errors=True)
+        with patch("hydrolib.tools.ext_old_to_new.main_converter.ExternalForcingConverter.save", return_value = None):
+            ext_old_to_new_dir_recursive(path, suppress_errors=True)
 
 
 class TestExternalFocingConverter:
