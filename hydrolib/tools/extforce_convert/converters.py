838cdf4a282fbf3717cf0db3749ebc5e1d49466e
Jeroen <jeroen.noort@deltares.nl>
2025-05-12 14:25:04 +0200

    fix code comments

--- hydrolib/tools/extforce_convert/converters.py
+++ hydrolib/tools/extforce_convert/converters.py
@@ -1196,10 +1196,11 @@ def update_extforce_file_new(
                     # Remainder of the line (after '=')
                     right_part = line[eq_index + 1 :].strip("\n")  # noqa: E203
                     name_len = len(new_forcing_filename)
-                    # Insert new filename immediately after '=' + a space
+                    # Protect against filename overflow into the comment
                     right_part_clipped = right_part[name_len + 1 :]
                     if right_part_clipped and right_part_clipped.find("#") == -1:
                         right_part_clipped = f"#{right_part_clipped}"
+                    # Insert new filename immediately after '=' + a space
                     new_line = (
                         f"{left_part} {new_forcing_filename}{right_part_clipped}\n"
                     )
