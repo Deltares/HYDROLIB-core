f2b690802d4a115bec85025b4a89888acc7eb0ca
Mostafa Farrag <moah.farag@gmail.com>
2025-05-28 21:07:11 +0200

    add populate_by_name=True to allow assigning fields by their names

--- hydrolib/core/dflowfm/ini/models.py
+++ hydrolib/core/dflowfm/ini/models.py
@@ -120,7 +120,7 @@ class INIBasedModel(BaseModel, ABC):
 
     _header: str = ""
     _file_path_style_converter = FilePathStyleConverter()
-    model_config = ConfigDict(extra="ignore", arbitrary_types_allowed=False)
+    model_config = ConfigDict(extra="ignore", arbitrary_types_allowed=False, populate_by_name=True)
 
     @classmethod
     def _get_unknown_keyword_error_manager(cls) -> Optional[UnknownKeywordErrorManager]:
