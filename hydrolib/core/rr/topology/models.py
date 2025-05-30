84514b938c9eeff51789fb98a576835eda897a17
Jeroen <jeroen.noort@deltares.nl>
2025-05-30 13:48:13 +0200

    fix aliasing in `topology`

--- hydrolib/core/rr/topology/models.py
+++ hydrolib/core/rr/topology/models.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Callable, Dict, List, Optional
+from typing import Callable, ClassVar, Dict, List, Optional
 
 from pydantic import Field, model_validator
 
@@ -57,18 +57,18 @@ class Node(BaseModel):
         kwargs["by_alias"] = True
         return super().model_dump(*args, **kwargs)
 
-    @model_validator(mode="before")
+    @model_validator(mode="after")
     @classmethod
-    def _validate_node_type(cls, values):
-
+    def _validate_node_type(cls, values: "Node") -> "Node":
+        values_dict = values.model_dump()
         cls._raise_if_invalid_type(
-            values,
-            "modelnodetype",
+            values_dict,
+            "mt",
             set(nodetypes_netter_to_rr.values()),
             "model node type (mt)",
         )
 
-        modelnodetype = values.get("modelnodetype")
+        modelnodetype = values_dict.get("mt")
 
         # modelnodetype=6 ("boundary node") is a special case that allows various netter nodetypes,
         # so therefore it always validates well.
@@ -76,13 +76,13 @@ class Node(BaseModel):
             return values
 
         cls._raise_if_invalid_type(
-            values,
-            "netternodetype",
+            values_dict,
+            "nt",
             set(nodetypes_netter_to_rr.keys()),
             "netter node type (nt)",
         )
 
-        netternodetype = values.get("netternodetype")
+        netternodetype = values_dict.get("nt")
         modelnodetype_expected = nodetypes_netter_to_rr[netternodetype]
 
         if modelnodetype != modelnodetype_expected:
@@ -122,8 +122,14 @@ class Node(BaseModel):
 class NodeFile(ParsableFileModel):
     """Represents the file with the RR node topology data."""
 
-    _parser = NetworkTopologyFileParser(enclosing_tag="node")
+    _parser: ClassVar[NetworkTopologyFileParser] = NetworkTopologyFileParser(
+        enclosing_tag="node"
+    )
     node: List[Node] = Field(default_factory=list, alias="node")
+
+    def model_dump(self, *args, **kwargs):
+        kwargs["by_alias"] = True
+        return super().model_dump(*args, **kwargs)
 
     @classmethod
     def _ext(cls) -> str:
@@ -167,8 +173,14 @@ class Link(BaseModel):
 class LinkFile(ParsableFileModel):
     """Represents the file with the RR link topology data."""
 
-    _parser = NetworkTopologyFileParser(enclosing_tag="brch")
+    _parser: ClassVar[NetworkTopologyFileParser] = NetworkTopologyFileParser(
+        enclosing_tag="brch"
+    )
     link: List[Link] = Field([], alias="brch")
+
+    def model_dump(self, *args, **kwargs):
+        kwargs["by_alias"] = True
+        return super().model_dump(*args, **kwargs)
 
     @classmethod
     def _ext(cls) -> str:
