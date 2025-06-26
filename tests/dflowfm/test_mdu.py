49f2906090060d0f6b792bdcad6446871d3cf81c
Jeroen <143803944+jeroen-deltares@users.noreply.github.com>
2025-06-26 15:58:59 +0200

    Task/732 migrate mdu to pydantic v2 (#852)
    * use pydantic v2 methods
    
    * create a resolver for any model object
    
    * use `model_dump`
    
    * fix file parsing for unsplit string lists.
    
    * add mdu test to ci
    
    * discern type of union `obsfile`
    
    * discern type of union `crsfile`
    
    * fix typo in dict key
    
    * change location validation to run after string split
    
    * fix expected error message
    
    * add drypointsfile resolver and refactor other union methods
    
    * refactor initializing of model union lists.
    
    * update reference file

--- tests/dflowfm/test_mdu.py
+++ tests/dflowfm/test_mdu.py
@@ -67,7 +67,7 @@ class TestModels:
         fmmodel.geometry.netfile.network = network
         importfm.geometry.netfile.network = network
 
-        assert importfm == fmmodel
+        assert importfm.model_dump() == fmmodel.model_dump()
 
     def test_mdu_file_with_network_is_read_correctly(self):
         input_mdu = (
@@ -407,7 +407,7 @@ class TestOutput:
         tmp_mdu_path = tmp_path / "tmp.mdu"
         tmp_mdu_path.write_text(tmp_mdu)
 
-        section_header = "General"
+        section_header = "default='General'"
         name = "unknownkey"
 
         expected_message = (
@@ -417,6 +417,7 @@ class TestOutput:
         with pytest.raises(ValueError) as exc_err:
             FMModel(filepath=tmp_mdu_path)
 
+        print(str(exc_err.value))
         assert expected_message in str(exc_err.value)
 
     def test_mdu_unknown_keywords_loading_throws_valueerror_for_unknown_keywords(
@@ -437,12 +438,14 @@ class TestOutput:
         tmp_mdu_path = tmp_path / "tmp.mdu"
         tmp_mdu_path.write_text(tmp_mdu)
 
-        section_header = "General"
+        section_header = "default='General'"
         name = "unknownkey"
         name2 = "unknownkey2"
 
-        expected_message = f"Unknown keywords are detected in section: '{section_header}', '{[name, name2]}'"
-
+        expected_message = (
+            "Unknown keywords are detected in section: "
+            f"'{section_header}', '{[name, name2]}'"
+        )
         with pytest.raises(ValueError) as exc_err:
             FMModel(filepath=tmp_mdu_path)
 
