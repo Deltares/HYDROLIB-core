a4b4a3b1f412ef41c194c3ddf1dc6f026b26c3b1
MAfarrag <MAfarrag@users.noreply.github.com>
2025-07-25 09:33:42 +0000

    autoformat: isort & black

--- hydrolib/tools/extforce_convert/converters.py
+++ hydrolib/tools/extforce_convert/converters.py
@@ -243,7 +243,9 @@ class BoundaryConditionConverter(BaseConverter):
         tim_model = self.merge_tim_files(tim_files, quantity)
 
         # switch the quantity names from the Tim model (loction names) to quantity names.
-        user_defined_names = BoundaryConditionConverter._get_file_labels(label, tim_files)
+        user_defined_names = BoundaryConditionConverter._get_file_labels(
+            label, tim_files
+        )
         tim_model.quantities_names = [quantity] * len(tim_model.get_units())
 
         units = tim_model.get_units()
@@ -390,7 +392,9 @@ class BoundaryConditionConverter(BaseConverter):
         t3d_models = [T3DModel(path) for path in t3d_files]
         # this line assumed that the two t3d files will have the same number of layers and same number of quantities
         quantities_names = [quantity] * t3d_models[0].size[1]
-        user_defined_names = BoundaryConditionConverter._get_file_labels(label, t3d_files)
+        user_defined_names = BoundaryConditionConverter._get_file_labels(
+            label, t3d_files
+        )
         t3d_forcing_list = T3DToForcingConverter.convert(
             t3d_models, quantities_names, user_defined_names
         )
@@ -415,7 +419,9 @@ class BoundaryConditionConverter(BaseConverter):
                 The converted ForcingModel.
         """
         cmp_models = [CMPModel(path) for path in cmp_files]
-        user_defined_names = BoundaryConditionConverter._get_file_labels(label, cmp_files)
+        user_defined_names = BoundaryConditionConverter._get_file_labels(
+            label, cmp_files
+        )
 
         for cmp_model in cmp_models:
             cmp_model.quantities_name = [quantity]
@@ -428,6 +434,7 @@ class BoundaryConditionConverter(BaseConverter):
         file_int_id = [int(file.stem.split("_")[-1]) for file in files]
         user_defined_names = [f"{label}_{str(i).zfill(4)}" for i in file_int_id]
         return user_defined_names
+
 
 class InitialConditionConverter(BaseConverter):
     """Initial condition converter."""
