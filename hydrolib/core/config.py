09abfb89717c275cdbf831c8d1b0229d001f55a6
Mostafa Farrag <moah.farag@gmail.com>
2025-10-21 10:28:49 +0200

    feat(config): add `pydantic-settings` for enhanced configuration management
    - Introduced `pydantic-settings` dependency in `pyproject.toml`.
    - Updated `Settings` class to support `.env` file configuration with UTF-8 encoding and unknown variable handling.

--- hydrolib/core/config.py
+++ hydrolib/core/config.py
@@ -1,11 +1,15 @@
-from pydantic.v1 import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """Configuration management, can be derived from ENV of .env file."""
 
+    model_config = SettingsConfigDict(
+            env_file=".env",            # read from .env if present
+            env_file_encoding="utf-8",  # typical default
+            extra="ignore"              # ignore unknown env vars
+    )
+
     FM_EXECUTABLE: str = "fm.exe"
 
 
-# TODO maybe delay init untill model init?
 settings = Settings()
