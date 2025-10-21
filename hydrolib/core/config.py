4b84ddbbc4aa62b5d2b781757421273fe15a8a13
MAfarrag <MAfarrag@users.noreply.github.com>
2025-10-21 08:31:06 +0000

    autoformat: isort & black

--- hydrolib/core/config.py
+++ hydrolib/core/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     """Configuration management, can be derived from ENV of .env file."""
 
     model_config = SettingsConfigDict(
-            env_file=".env",            # read from .env if present
-            env_file_encoding="utf-8",  # typical default
-            extra="ignore"              # ignore unknown env vars
+        env_file=".env",  # read from .env if present
+        env_file_encoding="utf-8",  # typical default
+        extra="ignore",  # ignore unknown env vars
     )
 
     FM_EXECUTABLE: str = "fm.exe"
