daea68725bde43283056b402009dfb7773a0e56a
Mostafa Farrag <moah.farag@gmail.com>
2025-11-06 09:03:54 +0100

    add substance models module

--- hydrolib/core/dflowfm/substances/models.py
+++
@@ -1,9 +0,0 @@
-"""models.py defines all classes and functions related to representing substance files.
-"""
-
-from strenum import StrEnum
-
-class SubstanceType(StrEnum):
-    """SubstanceType defines the type of substance."""
-    Active = "Active"
-    Inactive = "Inactive"
