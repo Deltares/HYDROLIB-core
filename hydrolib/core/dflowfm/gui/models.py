a27ef239a82df1f868d9b366cc03613965c60edf
MAfarrag <MAfarrag@users.noreply.github.com>
2026-04-02 09:12:04 +0000

    autoformat: isort & black

--- hydrolib/core/dflowfm/gui/models.py
+++ hydrolib/core/dflowfm/gui/models.py
@@ -117,7 +117,9 @@ class BranchModel(INIModel):
     """
 
     general: BranchGeneral = BranchGeneral()
-    branch: Annotated[List[Branch], BeforeValidator(ensure_list)] = Field(default_factory=list)
+    branch: Annotated[List[Branch], BeforeValidator(ensure_list)] = Field(
+        default_factory=list
+    )
 
     @classmethod
     def _ext(cls) -> str:
