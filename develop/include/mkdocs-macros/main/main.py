6aaa1bcb9ff4bf00e85cf44d2e5adb0ba7ec35e2
MAfarrag <MAfarrag@users.noreply.github.com>
2026-05-04 12:22:58 +0000

    Deployed b79f1f68 to develop with MkDocs 1.6.1 and mike 2.1.4

--- develop/include/mkdocs-macros/main/main.py
+++ develop/include/mkdocs-macros/main/main.py
@@ -1,7 +1,8 @@
+"""Mkdocs-macros definitions for the HYDROLIB-core documentation site."""
+
+
 def define_env(env):
-    """
-    Set up specific environment for documentation site,
-    with some convenience macro's.
+    """Set up specific environment for documentation site, with some convenience macro's.
 
     Available custom mkdocs-macros that produce Markdown content:
     { sobek_um(anchor, linktext) }: (deep)link to SOBEK User Manual PDF.
@@ -9,9 +10,7 @@ def define_env(env):
     { gh_issue(number, linktext) }: link to GitHub issue page.
     { gh_pr(number, linktext) }: link to GitHub pull request page.
     { gh_repo(path, linktext) }: link to a file's page in the GitHub repo.
-
     """
-
     dflowfm_um_url = (
         "https://content.oss.deltares.nl/delft3dfm1d2d/D-Flow_FM_User_Manual_1D2D.pdf"
     )
@@ -24,19 +23,16 @@ def define_env(env):
     @env.macro
     def dflowfm_um(anchor: str = "", linktext: str = "D-Flow FM UM"):
         """Create Markdown link for D-Flow FM User Manual, with optional anchor/bookmark."""
-
         return "[" + linktext + "](" + _get_url(dflowfm_um_url, anchor) + ")"
 
     @env.macro
     def sobek_um(anchor: str = "", linktext: str = "SOBEK UM"):
         """Create Markdown link for SOBEK User Manual, with optional anchor/bookmark."""
-
         return "[" + linktext + "](" + _get_url(sobek_um_url, anchor) + ")"
 
     @env.macro
     def gh_issue(number: int, linktext: str = None):
         """Create Markdown link to GitHub issue page."""
-
         return (
             "["
             + (linktext or "#" + str(number))
@@ -50,7 +46,6 @@ def define_env(env):
     @env.macro
     def gh_pr(number: int, linktext: str = None):
         """Create Markdown link to GitHub pull request page."""
-
         return (
             "["
             + (linktext or "#" + str(number))
@@ -64,7 +59,6 @@ def define_env(env):
     @env.macro
     def gh_repo(path: str, linktext: str = None):
         """Create Markdown link to a file's page in the GitHub repo."""
-
         return (
             "["
             + (linktext or "#" + path)
