be8218c6a286813a43e8478d3fd73bef5a06d03c
Radu Iordache <radu.iordache@deltares.nl>
2026-07-03 15:29:58 +0200

    fix(init): remove legacy operand map from exports

--- hydrolib/core/dflowfm/common/__init__.py
+++ hydrolib/core/dflowfm/common/__init__.py
@@ -1,5 +1,5 @@
 """Common model types shared across D-Flow FM file formats."""
 
-from .models import LocationType, Operand, _OPERAND_LEGACY_MAP
+from .models import LocationType, Operand
 
-__all__ = ["LocationType", "Operand", "_OPERAND_LEGACY_MAP"]
+__all__ = ["LocationType", "Operand"]
