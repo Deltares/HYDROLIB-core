e7efe5ff4d7d59da237c2ef063e08cd2e2759ee7
Radu Iordache <radu.iordache@deltares.nl>
2026-08-12 16:44:30 +0200

    fix(models): remove return type annotation from operand validation method

--- hydrolib/core/dflowfm/ext/models.py
+++ hydrolib/core/dflowfm/ext/models.py
@@ -681,7 +681,7 @@ class Meteo(INIBasedModel):
 
     @field_validator("operand", mode="before")
     @classmethod
-    def validate_operand(cls, v: Any) -> Operand:
+    def validate_operand(cls, v: Any):
         return enum_value_parser(v, Operand, _OPERAND_LEGACY_MAP)
 
 
